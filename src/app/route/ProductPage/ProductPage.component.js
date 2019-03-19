0c1a153ab7091b9e8d644b36f97c7167ff594aad
AInārs Sondors <ainars.sondors@scandiweb.com>
2019-03-19 15:30:33 +0200

    Page-Dying  -Fixed CR comments

--- src/app/route/ProductPage/ProductPage.component.js
+++ src/app/route/ProductPage/ProductPage.component.js
@@ -98,9 +98,9 @@ class ProductPage extends Component {
      */
     getThumbnail(currentVariantIndex, dataSource) {
         const { thumbnail, variants } = dataSource;
-        const variantThumbnail = (variants && variants[ currentVariantIndex ])
-            ? variants[ currentVariantIndex ].product.thumbnail
-            : null;
+        const variantThumbnail = variants
+            && variants[ currentVariantIndex ]
+            && variants[ currentVariantIndex ].product.thumbnail;
         return variantThumbnail || thumbnail;
     }
 
