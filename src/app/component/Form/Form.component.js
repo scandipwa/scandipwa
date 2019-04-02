5d512c1262e818921fa37674964b1a34fba5ac1a
Alfreds Genkins <alfreds@scandiweb.com>
2019-04-02 14:41:10 +0300

    Fixed Form bahviour for dynamic form content.

--- src/app/component/Form/Form.component.js
+++ src/app/component/Form/Form.component.js
@@ -14,25 +14,36 @@ import PropTypes from 'prop-types';
 import valdationConfig from './Form.config';
 
 class Form extends Component {
-    constructor(props) {
-        super(props);
-
-        const { children: propsChildren } = this.props;
-
-        this.refMap = {};
-
+    static updateChildrenRefs(props) {
+        const { children: propsChildren } = props;
+        const refMap = {};
         const children = Children.map(propsChildren, (child) => {
             const { type: { name }, props: { id } } = child;
 
             if (name === 'Field') {
-                this.refMap[id] = React.createRef();
-                return React.cloneElement(child, { formRef: this.refMap[id] });
+                refMap[id] = React.createRef();
+                return React.cloneElement(child, { formRef: refMap[id] });
             }
 
             return child;
         });
 
-        this.state = { children };
+        return { children, refMap };
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            ...Form.updateChildrenRefs(props),
+            fieldsAreValid: true
+        };
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        const { fieldsAreValid } = state;
+        if (fieldsAreValid) return Form.updateChildrenRefs(props);
+        return null;
     }
 
     addExtraProps(component, extraProps) {
@@ -40,10 +51,11 @@ class Form extends Component {
     }
 
     validateField(field) {
+        const { refMap } = this.state;
         const { validation, id } = field.props;
 
-        if (validation && id && this.refMap[id] && this.refMap[id].current) {
-            const { current: inputNode } = this.refMap[id];            
+        if (validation && id && refMap[id] && refMap[id].current) {
+            const { current: inputNode } = refMap[id];
 
             for (let i = 0; i < validation.length; i++) {
                 const rule = validation[i];
@@ -61,11 +73,11 @@ class Form extends Component {
     }
 
     handleFormSubmit(e) {
-        e.preventDefault();
-
-        let fieldsAreValid = true;
-
+        const { refMap } = this.state;
         const { children: propsChildren, onSubmit, onError } = this.props;
+        const invalidFields = [];
+
+        e.preventDefault();
 
         const children = Children.map(propsChildren, (child) => {
             const { type: { name }, props: { id } } = child;
@@ -74,30 +86,42 @@ class Form extends Component {
                 const { message } = this.validateField(child);
 
                 if (message) {
-                    fieldsAreValid = false;
-                    return this.addExtraProps(
-                        child,
-                        {
-                            message,
-                            formRef: this.refMap[id]
-                        }
-                    );
+                    invalidFields.push(id);
+                    return this.addExtraProps(child, { message, formRef: refMap[id] });
                 }
+
+                return this.addExtraProps(child, { formRef: refMap[id] });
             }
 
             return child;
         });
 
-        this.setState({ children });
+        this.setState({ children, fieldsAreValid: !invalidFields.length });
 
-        return fieldsAreValid ? onSubmit() : onError();
+        const inputValues = Object.values(refMap).reduce((inputValues, input) => {
+            const { current } = input;
+            if (current && current.id && current.value) {
+                const { id, value } = current;
+                return { ...inputValues, [id]: value };
+            }
+            return inputValues;
+        }, {});
+
+        return !invalidFields.length
+            ? onSubmit(inputValues)
+            : onError(inputValues, invalidFields);
     }
 
     render() {
-        const { children } = this.state;
+        const { children, fieldsAreValid } = this.state;
 
         return (
-            <form ref={ this.form } onSubmit={ e => this.handleFormSubmit(e) }>
+            <form
+              block="Form"
+              mods={ { isInvalid: !fieldsAreValid } }
+              ref={ this.form }
+              onSubmit={ e => this.handleFormSubmit(e) }
+            >
                 { children }
             </form>
         );
