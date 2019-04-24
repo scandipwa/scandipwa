a3d0bd099b50f1c6abdeccb50bc67f5bfc53ca20
Alfreds Genkins <alfreds@scandiweb.com>
2019-04-24 19:22:16 +0300

    Styled search overlay.

--- src/app/component/SearchOverlay/SearchOverlay.component.js
+++ src/app/component/SearchOverlay/SearchOverlay.component.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Image from 'Component/Image';
 import Overlay from 'Component/Overlay';
+import { ItemsType } from 'Type/ProductList';
 import './SearchOverlay.style';
+import TextPlaceholder from 'Component/TextPlaceholder';
 
 class SearchOverlay extends Component {
-    requestSearchBar() {
+    constructor(props) {
+        super(props);
 
+        this.handleItemClick = this.handleItemClick.bind(this);
     }
 
     componentDidUpdate(prevProps) {
@@ -16,28 +22,135 @@ class SearchOverlay extends Component {
 
         if (searchCriteria !== prevSearchCriteria) {
             this.timeout = setTimeout(() => {
-                this.requestSearchBar();
+                this.makeSearchRequest();
             }, 500);
         }
     }
 
-    render() {
+    getProductLinkTo(product) {
+        const { url_key, configurableVariantIndex, parent } = product;
+        const variantIndex = configurableVariantIndex || 0;
+
+        if (!url_key) return '/';
+
+        return {
+            pathname: `/product/${ url_key }`,
+            state: { product: parent || product, variantIndex },
+            search: `?variant=${ variantIndex }`
+        };
+    }
+
+    handleItemClick() {
+        const { hideActiveOverlay } = this.props;
+        hideActiveOverlay();
+    }
+
+    makeSearchRequest() {
+        const { makeSearchRequest, clearSearchResults, searchCriteria } = this.props;
+
+        if (searchCriteria) {
+            clearSearchResults();
+            makeSearchRequest({ search: searchCriteria });
+        }
+    }
+
+    renderSearchItem(product, i) {
+        const {
+            thumbnail,
+            name,
+            brand
+        } = product;
+
+        const imageSrc = thumbnail ? `/media/catalog/product${ thumbnail.path }` : '';
+
+        return (
+            <li
+              block="SearchOverlay"
+              elem="Item"
+              key={ i }
+            >
+                <Link to={ this.getProductLinkTo(product) } onClick={ this.handleItemClick }>
+                    <figure
+                      block="SearchOverlay"
+                      elem="Wrapper"
+                    >
+                        <Image
+                          block="SearchOverlay"
+                          elem="Image"
+                          src={ imageSrc }
+                          alt={ `Product ${name} thumbnail.` }
+                        />
+                        <figcaption block="SearchOverlay" elem="Content">
+                            <p block="SearchOverlay" elem="Brand"><TextPlaceholder content={ brand } /></p>
+                            <h4 block="SearchOverlay" elem="Title"><TextPlaceholder content={ name } length="long" /></h4>
+                        </figcaption>
+                    </figure>
+                </Link>
+            </li>
+        );
+    }
+
+    renderSearchCriteria() {
         const { searchCriteria } = this.props;
 
+        return (
+            <p
+              block="SearchOverlay"
+              elem="Criteria"
+              mods={ { isVisible: !!searchCriteria } }
+            >
+                Results for:
+                <strong>{ searchCriteria }</strong>
+            </p>
+        );
+    }
+
+    renderSearchResults() {
+        const { searchCriteria, searchResults, isLoading } = this.props;
+
+        if (!searchCriteria) {
+            return (<p>Start typing to see search results!</p>);
+        }
+
+        if (!searchResults.length && !isLoading) {
+            return (<p>No results found!</p>);
+        }
+
+        const resultsToRender = isLoading ? Array(5).fill({}) : searchResults;
+
+        return (
+            <ul>
+                { resultsToRender.map((item, i) => this.renderSearchItem(item, i)) }
+            </ul>
+        );
+    }
+
+    render() {
         return (
             <Overlay
               id="search"
               mix={ { block: 'SearchOverlay' } }
             >
-                Searching for:
-                <span>{ searchCriteria }</span>
+                { this.renderSearchCriteria() }
+                <article
+                  block="SearchOverlay"
+                  elem="Results"
+                  aria-label="Search results"
+                >
+                    { this.renderSearchResults() }
+                </article>
             </Overlay>
         );
     }
 }
 
 SearchOverlay.propTypes = {
-    searchCriteria: PropTypes.string
+    hideActiveOverlay: PropTypes.func.isRequired,
+    makeSearchRequest: PropTypes.func.isRequired,
+    clearSearchResults: PropTypes.func.isRequired,
+    searchCriteria: PropTypes.string,
+    searchResults: ItemsType.isRequired,
+    isLoading: PropTypes.bool.isRequired
 };
 
 SearchOverlay.defaultProps = {
--- src/app/component/SearchOverlay/SearchOverlay.connect.js
+++ src/app/component/SearchOverlay/SearchOverlay.connect.js
@@ -1,19 +1,17 @@
 import { connect } from 'react-redux';
 import { SearchBarDispatcher } from 'Store/SearchBar';
+import { hideActiveOverlay } from 'Store/Overlay';
 import SearchOverlay from './SearchOverlay.component';
 
 const mapStateToProps = state => ({
-    products: state.SearchBarReducer.productsInSearch,
+    searchResults: state.SearchBarReducer.productsInSearch,
     isLoading: state.SearchBarReducer.isLoading
 });
 
 const mapDispatchToProps = dispatch => ({
-    requestSearchBar: (options) => {
-        SearchBarDispatcher.handleData(dispatch, options);
-    },
-    clearSearchResults: () => {
-        SearchBarDispatcher.clearSearchResults(dispatch);
-    }
+    hideActiveOverlay: () => dispatch(hideActiveOverlay()),
+    makeSearchRequest: options => SearchBarDispatcher.handleData(dispatch, options),
+    clearSearchResults: () => SearchBarDispatcher.clearSearchResults(dispatch)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchOverlay);
