f027deac5083a92735ce8803d2eb96119182b882
Dmitrijs Asanovs <dmitrijs.asanovs@scandiweb.com>
2019-05-03 17:00:24 +0300

    change and align color block

--- src/app/component/ProductActions/ProductActions.component.js
+++ src/app/component/ProductActions/ProductActions.component.js
@@ -131,6 +131,25 @@ class ProductActions extends Component {
         );
     }
 
+    renderShortDescription() {
+        const { product: { short_description } } = this.props;
+
+        if (!short_description) return null;
+
+        const { html } = short_description;
+
+        return (
+            <section
+              block="ProductActions"
+              elem="Section"
+              mods={ { type: 'short-description' } }
+              aria-label="Product short description"
+            >
+                <Html content={ html } />
+            </section>
+        );
+    }
+
     renderShortProductInformation() {
         const { product: { brand, short_description, name } } = this.props;
 
@@ -282,6 +301,7 @@ class ProductActions extends Component {
                 { this.renderOtherOptions() }
                 { this.renderShortProductInformation() }
                 { this.renderSkuAndStock() }
+                { this.renderShortDescription() }
                 { this.renderAdditionalInformation() }
                 { this.renderRelatedProducts() }
             </article>
