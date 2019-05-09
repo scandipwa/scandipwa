fd63e835e63b48b4e0359c06a84fc446dcb2c0a0
Yefim Butrameev <yefim.butrameev@scandiweb.com>
2019-05-09 16:58:45 +0300

    OneSignal notifications support

---
+++ src/public/public/OneSignalSDKUpdaterWorker.js
@@ -0,0 +1,2 @@
+importScripts('https://demo.scandipwa.com/sw.js');
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
--- src/public/public/pushcrew-sw.js
+++
@@ -1,2 +0,0 @@
-var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/32ca222330bbfbe7ff6c7b50dc1d66d9.js');
