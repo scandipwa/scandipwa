f0f73bb19e7c35b834eb805a0e37648efd949636
Ilja Lapkovskis <ilja@scandi.com>
2019-08-14 17:23:55 +0300

    Fix 'proceed to checkout' validation, display link as button

--- src/app/component/CartSummary/CartSummary.componet.js
+++ src/app/component/CartSummary/CartSummary.componet.js
@@ -38,7 +38,7 @@ class CartSummary extends Component {
                 subtotal, tax_amount, grand_total, shipping_amount, items = []
             }
         } = this.props;
-        const cartIsEmpty = items.length;
+        const cartIsEmpty = !items.length;
 
         // eslint-disable-next-line no-param-reassign, no-return-assign
         const itemsTax = items ? items.reduce((sum, { tax_amount }) => sum += tax_amount, tax_amount) : 0;
@@ -52,7 +52,7 @@ class CartSummary extends Component {
                     { shipping_amount && this.renderPriceLine(shipping_amount, __('Shipping'), { divider: true }) }
                     { this.renderPriceLine(grand_total, __('Order Total')) }
                 </ul>
-                <Link to="/checkout/shipping" disabled={ cartIsEmpty }>{ __('Proceed to checkout') }</Link>
+                <Link className="Button" to="/checkout/shipping" disabled={ cartIsEmpty }>{ __('Proceed to checkout') }</Link>
                 <Link to="/">{ __('Continue shopping') }</Link>
             </div>
         );
