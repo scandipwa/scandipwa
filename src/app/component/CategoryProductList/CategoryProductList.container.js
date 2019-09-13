d97636bd388db0c97ff704a9695d4607423203ef
Artjoms Travkovs <artjoms.travkovs@scandiweb.com>
2019-09-13 18:05:11 +0300

    Fixed major bugs after merge

--- src/app/component/CategoryProductList/CategoryProductList.container.js
+++ src/app/component/CategoryProductList/CategoryProductList.container.js
@@ -1,101 +1,27 @@
-import { PureComponent } from 'react';
-import { withRouter } from 'react-router-dom';
-import { getQueryParam } from 'Util/Url';
-import PropTypes from 'prop-types';
-import { PagesType } from 'Type/ProductList';
-import CategoryProductList from './CategoryProductList.component';
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/base-theme
+ * @link https://github.com/scandipwa/base-theme
+ */
+import { connect } from 'react-redux';
+import ProductList from 'Component/ProductList';
+import { ProductListDispatcher, updateLoadStatus } from 'Store/ProductList';
 
-export class CategoryProductListContainer extends PureComponent {
-    static propTypes = {
-        location: PropTypes.shape({
-            pathname: PropTypes.string.isRequired
-        }).isRequired,
-        pages: PagesType.isRequired,
-        loadPage: PropTypes.func.isRequired,
-        isLoading: PropTypes.bool.isRequired,
-        totalPages: PropTypes.number.isRequired
-    };
+export const mapStateToProps = state => ({
+    pages: state.ProductListReducer.pages,
+    isLoading: state.ProductListReducer.isLoading,
+    totalItems: state.ProductListReducer.totalItems,
+    totalPages: state.ProductListReducer.totalPages
+});
 
-    state = { pagesCount: 1 };
+export const mapDispatchToProps = dispatch => ({
+    requestProductList: options => ProductListDispatcher.handleData(dispatch, options),
+    updateLoadStatus: isLoading => dispatch(updateLoadStatus(isLoading))
+});
 
-    componentDidMount() {
-        const { pages } = this.props;
-        const { pagesCount } = this.state;
-        const pagesLength = Object.keys(pages).length;
-
-        if (pagesCount !== pagesLength) {
-            this.setState({ pagesCount: pagesLength });
-        }
-    }
-
-    static getDerivedStateFromProps(props) {
-        const { isLoading } = props;
-        if (isLoading) return { pagesCount: 1 };
-        return null;
-    }
-
-    containerProps = () => ({
-        currentPage: this._getPageFromUrl(),
-        isShowLoading: this._isShowLoading(),
-        isVisible: this._isVisible()
-    });
-
-    containerFunctions = () => ({
-        loadPrevPage: this.loadPage.bind(this, false),
-        loadPage: this.loadPage
-    });
-
-    loadPage = (next = true) => {
-        const { pagesCount } = this.state;
-        const { loadPage, totalPages, isLoading } = this.props;
-        const { minPage, maxPage, loadedPagesCount } = this._getPagesBounds();
-
-        const isUpdatable = totalPages > 0 && pagesCount === loadedPagesCount;
-        const shouldUpdateList = next ? maxPage < totalPages : minPage > 1;
-
-        if (isUpdatable && shouldUpdateList && !isLoading) {
-            this.setState({ pagesCount: pagesCount + 1 });
-            loadPage(next ? maxPage + 1 : minPage - 1);
-        }
-    };
-
-    _getPageFromUrl() {
-        const { location } = this.props;
-        return +(getQueryParam('page', location) || 1);
-    }
-
-    _getPagesBounds() {
-        const { pages } = this.props;
-        const keys = Object.keys(pages);
-
-        return {
-            maxPage: Math.max(...keys),
-            minPage: Math.min(...keys),
-            loadedPagesCount: keys.length
-        };
-    }
-
-    _isShowLoading() {
-        const { isLoading } = this.props;
-        const { minPage } = this._getPagesBounds();
-        return minPage > 1 && !isLoading;
-    }
-
-    _isVisible() {
-        const { totalPages } = this.props;
-        const { maxPage } = this._getPagesBounds();
-        return maxPage < totalPages;
-    }
-
-    render() {
-        return (
-            <CategoryProductList
-              { ...this.props }
-              { ...this.containerProps() }
-              { ...this.containerFunctions() }
-            />
-        );
-    }
-}
-
-export default withRouter(CategoryProductListContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
