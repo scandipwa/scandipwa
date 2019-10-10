228e235331ee7db788d1c3d505cf1f5e7181bd34
Dmitrijs Voronovs <dmitrijs.voronovs@scandiweb.com>
2019-10-10 18:42:59 +0200

    Changed key for delivery options

--- src/app/component/CheckoutDeliveryOptions/CheckoutDeliveryOptions.component.js
+++ src/app/component/CheckoutDeliveryOptions/CheckoutDeliveryOptions.component.js
@@ -6,15 +6,11 @@ import CheckoutDeliveryOption from 'Component/CheckoutDeliveryOption';
 
 import './CheckoutDeliveryOptions.style';
 
-export const FLAT_RATE = 'flatrate';
-
 class CheckoutDeliveryOptions extends PureComponent {
     static propTypes = {
         shippingMethods: shippingMethodsType.isRequired,
         selectShippingMethod: PropTypes.func.isRequired,
-        selectedShippingMethodCode: PropTypes.oneOf([
-            FLAT_RATE
-        ])
+        selectedShippingMethodCode: PropTypes.string
     };
 
     static defaultProps = {
@@ -37,12 +33,12 @@ class CheckoutDeliveryOptions extends PureComponent {
             selectShippingMethod
         } = this.props;
 
-        const { carrier_code } = option;
-        const isSelected = selectedShippingMethodCode === carrier_code;
+        const { method_code } = option;
+        const isSelected = selectedShippingMethodCode === method_code;
 
         return (
             <CheckoutDeliveryOption
-              key={ carrier_code }
+              key={ method_code }
               isSelected={ isSelected }
               option={ option }
               onClick={ selectShippingMethod }
