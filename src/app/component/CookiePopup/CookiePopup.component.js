574aacffd79eb17013a8dda1f1f4ef07cc35e087
Teodor Moquist <tm@scandesignmedia.dk>
2019-12-12 10:13:35 +0100

    Fixed indent + Fixed return null

--- src/app/component/CookiePopup/CookiePopup.component.js
+++ src/app/component/CookiePopup/CookiePopup.component.js
@@ -1,13 +1,13 @@
 /**
- * ScandiPWA - Progressive Web App for Magento
- *
- * Copyright © Scandiweb, Inc. All rights reserved.
- * See LICENSE for license details.
- *
- * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
- * @package scandipwa/base-theme
- * @link https://github.com/scandipwa/base-theme
- */
+* ScandiPWA - Progressive Web App for Magento
+*
+* Copyright © Scandiweb, Inc. All rights reserved.
+* See LICENSE for license details.
+*
+* @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+* @package scandipwa/base-theme
+* @link https://github.com/scandipwa/base-theme
+*/
 
 import { PureComponent } from 'react';
 import BrowserDatabase from 'Util/BrowserDatabase';
@@ -17,40 +17,42 @@ import './CookiePopup.style';
 export const COOKIE_POPUP = 'cookie_popup';
 
 class CookiePopup extends PureComponent {
-	constructor() {
-		super();
+    constructor() {
+        super();
 
-		this.state = {
-			isAccepted: BrowserDatabase.getItem(COOKIE_POPUP) || false
-		};
+        this.state = {
+            isAccepted: BrowserDatabase.getItem(COOKIE_POPUP) || false
+        };
 
-		this.acceptCookies = this.acceptCookies.bind(this);
-	}
+        this.acceptCookies = this.acceptCookies.bind(this);
+    }
 
-	acceptCookies() {
+    acceptCookies() {
 		BrowserDatabase.setItem(true, COOKIE_POPUP);
 		this.setState({ isAccepted: true });
 	}
 
-  	render() {
-		const { isAccepted } = this.state;
+    render() {
+        const { isAccepted } = this.state;
 
-		return (
-			<div block="CookiePopup" mods={ { isAccepted: isAccepted } }>
-				<div block="CookiePopup" elem="Content">
-					<p>
-						{ __('We use cookies, also from third parties, to remember your settings, statistics and targeted marketing. You accept all cookies by clicking further or by clicking "accept". Read more about our cookies and their purpose in our Cookie Policy - ') }
-						<Link to="/page/privacy-policy-cookie-restriction-mode">{ __('read more') }</Link>
-					</p>
-				</div>
-				<div block="CookiePopup" elem="CTA">
-					<button block="Button" onClick={ this.acceptCookies }>
-						{ __('Accept') }
-					</button>
-				</div>
-			</div>
-		);
-  	}
+        if (isAccepted) return null;
+
+        return (
+            <div block="CookiePopup">
+                <div block="CookiePopup" elem="Content">
+                    <p>
+                        { __('We use cookies, also from third parties, to remember your settings, statistics and targeted marketing. You accept all cookies by clicking further or by clicking "accept". Read more about our cookies and their purpose in our Cookie Policy - ') }
+                        <Link to="/page/privacy-policy-cookie-restriction-mode">{ __('read more') }</Link>
+                    </p>
+                </div>
+                <div block="CookiePopup" elem="CTA">
+                    <button block="Button" onClick={ this.acceptCookies }>
+                        { __('Accept') }
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default CookiePopup;
