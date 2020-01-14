ed5f11be718d43947e405ba063361a5c943f5110
krystian15 <31726767+krystian15@users.noreply.github.com>
2020-01-14 12:36:34 +0100

    Create index.js

---
+++ src/app/store/ContactForm/index.js
@@ -0,0 +1,14 @@
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/base-theme
+ * @link https://github.com/scandipwa/base-theme
+ */
+
+export * from './ContactForm.action';
+export { default as ContactFormReducer } from './ContactForm.reducer';
+export { default as ContactFormDispatcher, ContactFormDispatcher as ContactFormDispatcherClass } from './ContactForm.dispatcher';
