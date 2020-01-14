9c8c8a5f9e78b5ad3e02d7c693f8ed039dfaac3f
krystian15 <31726767+krystian15@users.noreply.github.com>
2020-01-14 12:34:47 +0100

    Create ContactForm.query.js

---
+++ src/app/query/ContactForm.query.js
@@ -0,0 +1,24 @@
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/base-theme
+ * @link https://github.com/scandipwa/base-theme
+ */
+
+import { Field } from 'Util/Query';
+
+class ContactFormQuery {
+
+    getSendContactFormMutation(options) {
+        return new Field('contactForm')
+            .addArgument('contact', 'ContactForm!', options)
+            .addField('message');
+    }
+}
+
+
+export default new ContactFormQuery();
