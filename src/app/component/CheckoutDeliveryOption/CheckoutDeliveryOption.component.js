2398e1471492c830584392f802d40fbc5d7ddf4f
Dmitrijs Voronovs <dmitrijs.voronovs@scandiweb.com>
2020-01-25 16:56:51 +0200

    price for shipping methods is rounded

--- src/app/component/CheckoutDeliveryOption/CheckoutDeliveryOption.component.js
+++ src/app/component/CheckoutDeliveryOption/CheckoutDeliveryOption.component.js
@@ -16,7 +16,7 @@ import { shippingMethodType } from 'Type/Checkout';
 import { TotalsType } from 'Type/MiniCart';
 
 import './CheckoutDeliveryOption.style';
-import { formatCurrency } from 'Util/Price';
+import { formatCurrency, roundPrice } from 'Util/Price';
 
 class CheckoutDeliveryOption extends PureComponent {
     static propTypes = {
@@ -45,9 +45,11 @@ class CheckoutDeliveryOption extends PureComponent {
             totals: { quote_currency_code }
         } = this.props;
 
+        const roundedUpPrice = roundPrice(price_incl_tax);
+
         return (
             <strong>
-                { ` - ${price_incl_tax}${formatCurrency(quote_currency_code)}` }
+                { ` - ${roundedUpPrice}${formatCurrency(quote_currency_code)}` }
             </strong>
         );
     }
