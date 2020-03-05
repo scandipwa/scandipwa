5e2426be9f5ed3ceb86341ab30669667b3c47dd6
yeegor <yegor.batov@gmail.com>
2020-03-05 19:51:08 +0200

    Add TODO: return helpers to plugin-transform-runtime because the issue this is a solution to is resolved by other approach

--- src/config/babel.config.js
+++ src/config/babel.config.js
@@ -23,6 +23,7 @@ const plugins = [
     'transform-rebem-jsx',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
+    // TODO: return helpers:true
     ['@babel/plugin-transform-runtime', { helpers: false }],
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-optional-chaining',
