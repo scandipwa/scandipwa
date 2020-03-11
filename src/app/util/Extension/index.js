09ac3fd129e50c386760475a438225aaa56eb2b2
yeegor <yegor.batov@gmail.com>
2020-03-11 20:54:14 +0200

    Fix linter issues

--- src/app/util/Extension/index.js
+++ src/app/util/Extension/index.js
@@ -23,7 +23,9 @@ export default function importExtensions(pendingPluginConfigParts) {
                                 throw Error(`Expected plugin handler type 'get' or 'construct', got ${handlerType}`);
                             }
                             if (targetType === 'instance' && handlerType !== 'get') {
-                                throw Error(`Expected handler type 'get' on target type 'instance', got ${handlerType}`);
+                                throw Error(
+                                    `Expected handler type 'get' on target type 'instance', got ${handlerType}`
+                                );
                             }
                             if (!overallConfig[namespace][targetType][handlerType]) {
                                 overallConfig[namespace][targetType][handlerType] = {};
