b3962d41d0cbd2d7aadef84d295a6e15c73267aa
Dmitrijs Voronovs <53301511+dmitrijs-voronovs@users.noreply.github.com>
2020-03-13 11:55:01 +0200

    [SWPWA-456] Add missing page titles (#560)
    * integrated OG metadata, fixed Meta component, integrated it into every page.
    
    * fix eslint
    
    * Fixed CR issues, added imageAlt prop
    
    * integrated Meta store
    
    * Base meta component
    
    * integrated store
    
    * changed update meta logic from overriding meta component to updating global store
    
    * removed unrelated fixes, opened new PR
    
    * Export functions
    
    * only menta support , no og tags' support
    
    * fix silly mistakes
    
    * remove render methods
    
    * Integrated meta dispatcher, lets only one action in meta store
    
    * fix eslint
    
    * removed react-helmet and replaced it with portal
    
    * added key to meta tags
    
    * Dynamic title with portals instead of hardcoded
    
    * No redundant methods
    
    * SomethingWentWrong title
    
    * SearchPage title
    
    * MenuPage title
    
    * NoMatchHandler title
    
    * PasswordChangePage title
    
    * SearchPage title (fix)
    
    * eslint issue fix
    
    Co-authored-by: Alfreds Genkins <alfreds@scandiweb.com>

--- src/app/route/MenuPage/MenuPage.container.js
+++ src/app/route/MenuPage/MenuPage.container.js
@@ -9,19 +9,29 @@
  * @link https://github.com/scandipwa/base-theme
  */
 
+import PropTypes from 'prop-types';
 import { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { updateMeta } from 'Store/Meta';
 import MenuOverlay from 'Component/MenuOverlay';
 import { HistoryType } from 'Type/Common';
 import { withRouter } from 'react-router';
 import isMobile from 'Util/Mobile';
 import './MenuPage.style';
 
+export const mapDispatchToProps = dispatch => ({
+    updateMeta: meta => dispatch(updateMeta(meta))
+});
+
 export class MenuPageContainer extends PureComponent {
     static propTypes = {
+        updateMeta: PropTypes.func.isRequired,
         history: HistoryType.isRequired
     };
 
     componentDidMount() {
+        const { updateMeta } = this.props;
+        updateMeta({ title: __('Menu') });
         this.redirectIfNotOnMobile();
     }
 
@@ -42,4 +52,4 @@ export class MenuPageContainer extends PureComponent {
     }
 }
 
-export default withRouter(MenuPageContainer);
+export default withRouter(connect(null, mapDispatchToProps)(MenuPageContainer));
