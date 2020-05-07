2507ed00f4b6c5ffb6ffec9695833c3c3cc7895f
AkimScandi <akim@scandiweb.com>
2020-05-07 14:01:47 +0300

    tmp commit

--- src/app/component/CartOverlay/CartOverlay.container.js
+++ src/app/component/CartOverlay/CartOverlay.container.js
@@ -15,36 +15,66 @@ import { PureComponent } from 'react';
 
 import { changeNavigationState, goToPreviousNavigationState } from 'Store/Navigation';
 import { TOP_NAVIGATION_TYPE } from 'Store/Navigation/Navigation.reducer';
-import { CART, CART_EDITING } from 'Component/Header';
-import { hideActiveOverlay } from 'Store/Overlay';
+import { CART, CART_EDITING, CUSTOMER_ACCOUNT } from 'Component/Header';
+import { CUSTOMER_ACCOUNT_OVERLAY_KEY } from 'Component/MyAccountOverlay/MyAccountOverlay.component';
+import { hideActiveOverlay, toggleOverlayByKey } from 'Store/Overlay';
 import { CartDispatcher } from 'Store/Cart';
 import { TotalsType } from 'Type/MiniCart';
+import isMobile from 'Util/Mobile';
+import { isSignedIn } from 'Util/Auth';
+import { history } from 'Route';
 
 import CartOverlay from './CartOverlay.component';
 
 export const mapStateToProps = state => ({
     totals: state.CartReducer.cartTotals,
-    guest_checkout: state.ConfigReducer.guest_checkout
+    guest_checkout: state.ConfigReducer.guest_checkout,
+    navigationState: state.NavigationReducer[TOP_NAVIGATION_TYPE].navigationState
 });
 
 export const mapDispatchToProps = dispatch => ({
     hideActiveOverlay: () => dispatch(hideActiveOverlay()),
-    goToPreviousHeaderState: () => dispatch(goToPreviousNavigationState(TOP_NAVIGATION_TYPE)),
+    setNavigationState: stateName => dispatch(changeNavigationState(TOP_NAVIGATION_TYPE, stateName)),
     changeHeaderState: state => dispatch(changeNavigationState(TOP_NAVIGATION_TYPE, state)),
-    updateTotals: options => CartDispatcher.updateTotals(dispatch, options)
+    updateTotals: options => CartDispatcher.updateTotals(dispatch, options),
+    showOverlay: overlayKey => dispatch(toggleOverlayByKey(overlayKey))
+
 });
 
 export class CartOverlayContainer extends PureComponent {
     static propTypes = {
         totals: TotalsType.isRequired,
-        changeHeaderState: PropTypes.func.isRequired
+        guest_checkout: PropTypes.bool.isRequired,
+        changeHeaderState: PropTypes.func.isRequired,
+        hideActiveOverlay: PropTypes.func.isRequired,
+        showOverlay: PropTypes.func.isRequired
     };
 
     state = { isEditing: false };
 
     containerFunctions = {
-        changeHeaderState: this.changeHeaderState.bind(this)
+        changeHeaderState: this.changeHeaderState.bind(this),
+        handleCheckoutClick: this.handleCheckoutClick.bind(this)
     };
+
+    handleCheckoutClick() {
+        const { guest_checkout, hideActiveOverlay, showOverlay, setNavigationState, navigationState: { name } } = this.props;
+        if (guest_checkout) {
+            window.location.href = "/checkout";
+        } else if (!guest_checkout) {
+            if (isSignedIn()) {
+                history.push({ pathname: '/checkout' });
+                return;
+            }
+
+            hideActiveOverlay();
+
+            if (!isMobile.any() && name !== CUSTOMER_ACCOUNT) {
+                showOverlay(CUSTOMER_ACCOUNT_OVERLAY_KEY);
+                setNavigationState({ name: CUSTOMER_ACCOUNT, title: 'Sign in' });
+            }
+        }
+    }
 
     changeHeaderState() {
         const { changeHeaderState, totals: { count = 0 } } = this.props;
