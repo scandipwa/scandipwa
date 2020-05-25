e9c5b8b32a28d06e86d973e1999e9d3dc62bc7a8
yeegor <yegor.batov@gmail.com>
2020-05-25 12:52:35 +0300

    Add apply handler to middleware function

--- src/config/Middleware/index.js
+++ src/config/Middleware/index.js
@@ -1,8 +1,79 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable arrow-body-style */
-/* eslint-disable func-names */
+/* eslint-disable */
+
+/**
+ * Sort plugins by position:
+ * @param {Array} plugins
+ * @param {string} errorText
+ */
+const sortPlugins = (plugins, errorText) => plugins
+    .sort((a, b) => {
+        if (a.position > b.position) return -1;
+        if (a.position < b.position) return 1;
+        throw new Error(errorText);
+    });
+
+/**
+ * Get error text to show if two plugins are defined with same positions
+ * @param {string} memberName
+ * @param {string|null} className
+ * @param {string} namespace
+ */
+const getHandlerErrorText = (memberName, className, namespace) => {
+    return `Cannot have equal position on different plugins of the same ${memberName} member
+    on ${ className ? `${className} class` : '' } in ${namespace} namespace!`;
+};
+
+/**
+ * Middlewaring given original member
+ * @param {Function} origMember
+ * @param {Array} sortedPlugins
+ * @param Context origContext
+ */
+function middlewareFunction(origMember, sortedPlugins, origContext) {
+    return function (...args) {
+        const starter = typeof origMember === 'function'
+            ? (...originalArgs) => origMember.apply(origContext, originalArgs)
+            : origMember;
+
+        const newMember = sortedPlugins.reduce(
+            (acc, { implementation }) => () => {
+                return typeof origMember === 'object'
+                    ? implementation(acc, origContext)
+                    : implementation(args, acc, origContext);
+            },
+            starter
+        );
+
+        return newMember(args);
+    };
+}
+
+/**
+ * Middleware function is supposed to wrap source classes
+ * in order to provide plugin functionality
+ * @param {Class} Class
+ * @param {string} namespace
+ */
 function middleware(Class, namespace) {
     Class.prototype.__namespace__ = namespace;
+
+    const applyHandler = function (origFunction, thisArg, originalArgs) {
+        const memberName = origFunction.prototype.constructor.name;
+
+        const memberPluginsApply = window.plugins?.[namespace]?.['function']?.['apply']?.[memberName];
+        if (!memberPluginsApply) {
+            return origFunction.apply(thisArg, originalArgs);
+        }
+
+        return middlewareFunction(
+            origFunction,
+            sortPlugins(
+                memberPluginsApply,
+                getHandlerErrorText(origFunction.prototype.constructor.name, null, namespace)
+            ),
+            thisArg
+        )();
+    };
 
     const getHandler = function (target, memberName, rec) {
         if (memberName === 'Symbol.iterator') {
@@ -15,31 +86,14 @@ function middleware(Class, namespace) {
             return origMember;
         }
 
-        // Sort plugins by priority
-        memberPluginsGet.sort((a, b) => {
-            if (a.position > b.position) return -1;
-            if (a.position < b.position) return 1;
-            throw new Error(
-                `Cannot have equal position on different plugins of the same ${memberName} member
-                on ${Object.getPrototypeOf(target).constructor.name} class in ${namespace} namespace!`
-            );
-        });
-
-        const middlewaredFunction = function (...args) {
-            const starter = typeof origMember === 'function'
-                ? (...originalArgs) => origMember.apply(this, originalArgs)
-                : origMember;
-
-            const newMember = memberPluginsGet.reduce(
-                (acc, { implementation }) => () => {
-                    return typeof origMember === 'object'
-                        ? implementation(acc, target)
-                        : implementation(args, acc, target);
-                }, starter
-            );
-
-            return newMember(args);
-        };
+        const middlewaredFunction = middlewareFunction(
+            origMember,
+            sortPlugins(
+                memberPluginsGet,
+                getHandlerErrorText(memberName, Object.getPrototypeOf(target).constructor.name, namespace)
+            ),
+            target
+        );
 
         if (typeof origMember === 'object') {
             return middlewaredFunction();
@@ -50,33 +104,56 @@ function middleware(Class, namespace) {
 
     const constructHandler = function (TargetClass, args) {
         const instance = new TargetClass(...args);
-        const namespacePluginsConstruct = window.plugins?.[namespace]?.['class']?.['construct'];
+        const namespacePluginsConstruct = window.plugins?.[namespace]?.['class']?.['construct'] || {};
 
-        Object.entries(namespacePluginsConstruct || {}).forEach(([memberName, memberPluginsConstruct]) => {
-            const origMember = instance[memberName];
-            memberPluginsConstruct.sort((a, b) => {
-                if (a.position > b.position) return 1;
-                if (a.position < b.position) return -1;
-                throw new Error(
-                    `Cannot have equal position on different plugins of the same\
-                    ${memberName} member on ${TargetClass.name} class in ${namespace} namespace!`
+        Object.entries(namespacePluginsConstruct).forEach(
+            ([memberName, memberPluginsConstruct]) => {
+                const origMember = instance[memberName];
+                const sortedPlugins = sortPlugins(
+                    memberPluginsConstruct,
+                    getHandlerErrorText(memberName, TargetClass.name, namespace)
                 );
-            });
 
-            const newMember = memberPluginsConstruct.reduce((acc, { implementation }) => {
-                return implementation(acc, instance);
-            }, origMember);
+                const newMember = sortedPlugins.reduce(
+                    (acc, { implementation }) => {
+                        return implementation(acc, instance);
+                    },
+                    origMember
+                );
 
-            instance[memberName] = newMember;
-        });
+                instance[memberName] = newMember;
+            }
+        );
 
         return instance;
     };
 
-    const handler = {
-        get: getHandler,
-        construct: constructHandler
-    };
+    const handler = {};
+
+    // All classes inherit from extensible classes
+    // ~ if `Class` inherits from class other than Object
+    // ~ if `Class` is class, not regular function
+    if (Class.prototype.__proto__.constructor.name === 'Object') {
+        // Apply handler for functions - intercepts function calls
+        Object.defineProperty(
+            handler,
+            'apply',
+            applyHandler
+        );
+    } else {
+        // Get handler for members - intercepts `get` calls, meant for class static members
+        Object.defineProperty(
+            handler,
+            'get',
+            getHandler
+        );
+        // Construct handler for classes - intercepts `new` operator calls, changes properties
+        Object.defineProperty(
+            handler,
+            'construct',
+            constructHandler
+        );
+    }
 
     return new Proxy(Class, handler);
 }
