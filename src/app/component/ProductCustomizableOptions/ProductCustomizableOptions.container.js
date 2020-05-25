fddfa663d52b1132da56ff957dd28b3d424c536d
Aivars <aivars.arbidans@scandiweb.com>
2020-05-25 16:37:53 +0300

    SWPWA-490 - Fixed eslint issue

--- src/app/component/ProductCustomizableOptions/ProductCustomizableOptions.container.js
+++ src/app/component/ProductCustomizableOptions/ProductCustomizableOptions.container.js
@@ -9,7 +9,7 @@
  * @link https://github.com/scandipwa/base-theme
  */
 
-import { PureComponent, createRef } from 'react';
+import { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { formatCurrency } from 'Util/Price';
