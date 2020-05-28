45a0c193c016010831ceb2f35e0ecc31c6396de7
Teodor Moquist <tm@scandesignmedia.dk>
2020-05-28 08:52:01 +0200

    Fixed guest_checkout bug

--- src/app/route/CartPage/CartPage.container.js
+++ src/app/route/CartPage/CartPage.container.js
@@ -110,7 +110,7 @@ export class CartPageContainer extends PureComponent {
         // to prevent outside-click handler trigger
         e.nativeEvent.stopImmediatePropagation();
 
-        if (!guest_checkout) {
+        if (guest_checkout) {
             history.push({ pathname: '/checkout' });
             return;
         }
