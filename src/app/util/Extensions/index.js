1ae5ae3265103024fff68d3b0d4789904b8b87cf
yeegor <yegor.batov@gmail.com>
2020-06-04 13:06:37 +0300

    Patch extensions config collector to allow config merging

--- src/app/util/Extensions/index.js
+++ src/app/util/Extensions/index.js
@@ -1,8 +1,6 @@
 /* eslint-disable */
 
-// eslint-disable-next-line import/prefer-default-export
 export const extensions = [];
-
 // The following line is a hook for extension-import-injector loader
 // See config/loaders/extension-import-injector
 // * ScandiPWA extension importing magic comment! */
@@ -12,6 +10,7 @@ const handlerMap = {
     'instance': [ 'get' ],
     'function': [ 'apply' ]
 };
+
 const handlerTypes = [...new Set(Object.values(handlerMap).flat())];
 const targetTypes = Object.keys(handlerMap);
 
@@ -45,43 +44,39 @@ window.plugins = extensions.reduce(
             }
             Object.entries(plugins).forEach(([targetType, handlerPlugins]) => {
                 validateTargetType(targetType);
-
                 if (!overallConfig[namespace][targetType]) {
                     overallConfig[namespace][targetType] = {};
                 }
                 Object.entries(handlerPlugins).forEach(([handlerType, membersPlugins]) => {
                     validateHandlerType(handlerType);
                     validateHandlerForTarget(targetType, handlerType);
-
-                    if (!overallConfig[namespace][targetType][handlerType]) {
-                        switch (handlerType) {
-                        // Handle reduced apply plugin config structure
-                        case 'apply':
+                    switch (handlerType) {
+                    // Handle reduced apply plugin config structure
+                    case 'apply':
+                        if (!overallConfig[namespace][targetType][handlerType]) {
                             overallConfig[namespace][targetType][handlerType] = [];
-                            membersPlugins.forEach((memberPlugin) => {
-                                overallConfig[namespace][targetType][handlerType].push(memberPlugin);
-                            })
-
-                            break;
-                        default:
-                            overallConfig[namespace][targetType][handlerType] = {};
-                            Object.entries(membersPlugins).forEach(([memberName, memberPlugins]) => {
-                                if (!overallConfig[namespace][targetType][handlerType][memberName]) {
-                                    overallConfig[namespace][targetType][handlerType][memberName] = [];
-                                }
-                                memberPlugins.forEach((memberPlugin) => {
-                                    overallConfig[namespace][targetType][handlerType][memberName].push(memberPlugin);
-                                });
-                            });
-
-                            break;
                         }
+                        membersPlugins.forEach((memberPlugin) => {
+                            overallConfig[namespace][targetType][handlerType].push(memberPlugin);
+                        })
+                        break;
+                    default:
+                        if (!overallConfig[namespace][targetType][handlerType]) {
+                            overallConfig[namespace][targetType][handlerType] = {}
+                        }
+                        Object.entries(membersPlugins).forEach(([memberName, memberPlugins]) => {
+                            if (!overallConfig[namespace][targetType][handlerType][memberName]) {
+                                overallConfig[namespace][targetType][handlerType][memberName] = [];
+                            }
+                            memberPlugins.forEach((memberPlugin) => {
+                                overallConfig[namespace][targetType][handlerType][memberName].push(memberPlugin);
+                            });
+                        });
+                        break;
                     }
-
                 });
             });
         });
-
         return overallConfig;
     }, {}
 );
