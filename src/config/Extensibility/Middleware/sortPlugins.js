db4a7842fb2f8a3952c6b0f32e24383c881e251b
yeegor <yegor.batov@gmail.com>
2020-06-15 18:57:52 +0300

    Make plugin position optional

--- src/config/Extensibility/Middleware/sortPlugins.js
+++ src/config/Extensibility/Middleware/sortPlugins.js
@@ -1,8 +1,16 @@
 /* eslint-disable */
+const DEFAULT_POSITION = 100;
 
 /**
  * Sort plugins by position so that ones with higher position get executed later
  * @param {Array} plugins
  * @param {string} errorText
  */
-module.exports = plugins => plugins.sort((a, b) => b - a);
+module.exports = plugins => plugins.sort(
+    (a, b) => {
+        const { position: positionA = DEFAULT_POSITION } = a;
+        const { position: positionB = DEFAULT_POSITION } = b;
+
+        return positionB - positionA;
+    }
+);
