1d23ed9279ec225169654a1b6b62c6daf7da7e74
yeegor <yegor.batov@gmail.com>
2020-06-18 18:05:39 +0300

    Fix some classes not being proxied properly

--- src/config/Extensibility/ExtensibleClasses/ProxyInstance.js
+++ src/config/Extensibility/ExtensibleClasses/ProxyInstance.js
@@ -3,8 +3,17 @@ const generateGetHandler = require('../Middleware/generateGetHandler');
 
 module.exports = function proxyInstance(context) {
     const { __namespace__ } = Object.getPrototypeOf(context);
+    const shouldProxyAnyway = () => {
+        return !![
+            // Because they are instantiated on export
+            __namespace__.includes('Query'),
+            __namespace__.includes('Dispatcher'),
+            __namespace__.includes('Util'),
+        ].find(Boolean);
+    }
+
     const namespacePlugins = globalThis.plugins?.[__namespace__]?.['member-function'];
-    if (!namespacePlugins) {
+    if (!namespacePlugins && !shouldProxyAnyway()) {
         return;
     }
 
