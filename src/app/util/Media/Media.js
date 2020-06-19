d035e0d58faa6fb3634c71c80090c850fd06414e
yeegor <yegor.batov@gmail.com>
2020-06-19 15:54:32 +0300

    Fix eslint

--- src/app/util/Media/Media.js
+++ src/app/util/Media/Media.js
@@ -3,6 +3,4 @@ export const CATEGORY_MEDIA = 'catalog/category/';
 export const PRODUCT_MEDIA = 'catalog/product';
 export const LOGO_MEDIA = 'logo/';
 
-export default (src, subPath = '') => {
-    return `${ window.secure_base_media_url || '/media/' }${ subPath }${ src }`;
-};
+export default (src, subPath = '') => `${ window.secure_base_media_url || '/media/' }${ subPath }${ src }`;
