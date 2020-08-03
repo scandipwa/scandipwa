4489a722574e32fbd90d99905ce9695a47bca1bc
yeegor <yegor.batov@gmail.com>
2020-08-03 12:41:20 +0300

    Proxy everything

--- src/config/Extensibility/ExtensibleClasses/ProxyInstance.js
+++ src/config/Extensibility/ExtensibleClasses/ProxyInstance.js
@@ -1,22 +1,10 @@
 /* eslint-disable */
 const generateGetHandler = require('../Middleware/generateGetHandler');
 
-const shouldProxyAnyway = (namespace) => {
-    // Because they are instantiated on export
-    return namespace.includes('Query')
-        || namespace.includes('Dispatcher')
-        || namespace.includes('Util');
-}
-
 module.exports = function proxyInstance(context) {
-    const { __namespace__: namespace } = Object.getPrototypeOf(context);
-
-    const namespacePlugins = globalThis.plugins?.[namespace]?.['member-function'];
-    if (!namespacePlugins && !shouldProxyAnyway(namespace)) {
-        return context;
-    }
+    const { __namespace__ } = Object.getPrototypeOf(context);
 
     return new Proxy(context, {
-        get: generateGetHandler('instance', namespace)
+        get: generateGetHandler('instance', __namespace__)
     });
 };
