1b0ddca7a3b16c6dcc9c5688a07895aa01decffe
yeegor <yegor.batov@gmail.com>
2020-08-07 15:24:08 +0300

    Comments + style improvements

--- src/config/Extensibility/Middleware/Extensible.js
+++ src/config/Extensibility/Middleware/Extensible.js
@@ -7,17 +7,24 @@ const cacheIdentityKey = Symbol('CacheIdentityKey');
 // Cache to optimise class generation
 const generated = [];
 
-module.exports = (BaseClass = class {}) => {
+// eslint-disable-next-line @scandipwa/scandipwa-guidelines/derived-class-names
+class EmptyBase {}
+
+module.exports = (BaseClass = EmptyBase) => {
+    // Handle already extensible classes
     if (BaseClass[extensible]) {
         return BaseClass;
     }
+
     const { name } = BaseClass;
     const {
-        [cacheIdentityKey]: cacheIdentity = Symbol(`CacheIdentity ${name}`)
+        // Generate unique cache identities as default value
+        [cacheIdentityKey]: cacheIdentity = Symbol(`Cache Identity ${name}`)
     } = BaseClass;
 
+    // If such class is not yet generated => generate the class
     if (!generated[cacheIdentity]) {
-        const generatedClass = class X extends BaseClass {
+        const GeneratedClass = class Extensible extends BaseClass {
             constructor(...args) {
                 super(...args);
                 const { __namespace__ } = Object.getPrototypeOf(this);
@@ -30,12 +37,12 @@ module.exports = (BaseClass = class {}) => {
             __construct() {}
         };
 
-        generatedClass[extensible] = true;
-        generated[cacheIdentity] = generatedClass;
+        GeneratedClass[extensible] = true;
+        generated[cacheIdentity] = GeneratedClass;
         Object.defineProperty(
             BaseClass,
             cacheIdentityKey,
-            { value: cacheIdentity }
+            { value: cacheIdentity, writable: false }
         );
     }
 
