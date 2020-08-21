50f1088d969ecc9b4e9b8acafbf64fd16dfd5dc6
yeegor <yegor.batov@gmail.com>
2020-08-21 15:49:12 +0300

    Fix license comment lack

--- src/app/util/History/index.js
+++ src/app/util/History/index.js
@@ -1,4 +1,15 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
+
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/base-theme
+ * @link https://github.com/scandipwa/base-theme
+ */
 import { createBrowserHistory } from 'history';
 
 export const history = createBrowserHistory({ basename: '/' });
