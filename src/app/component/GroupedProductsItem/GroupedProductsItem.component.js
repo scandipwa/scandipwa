9ba8266dfdf5144c200968cd269949cbc12b25a7
yeegor <yegor.batov@gmail.com>
2020-09-08 19:00:01 +0300

    Fix render quantity not displaying for grouped products

--- src/app/component/GroupedProductsItem/GroupedProductsItem.component.js
+++ src/app/component/GroupedProductsItem/GroupedProductsItem.component.js
@@ -94,7 +94,7 @@ export class GroupedProductsItem extends PureComponent {
             <li block="GroupedProductsItem" aria-label="Product Item">
                 { this.renderImage() }
                 { this.renderTitle() }
-                { this.renderQuantity }
+                { this.renderQuantity() }
             </li>
         );
     }
