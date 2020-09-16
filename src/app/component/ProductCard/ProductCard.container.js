e05bb1177c0a0f3224432557aabcee221e285613
yeegor <yegor.batov@gmail.com>
2020-09-16 17:24:17 +0300

    Improve code readability

--- src/app/component/ProductCard/ProductCard.container.js
+++ src/app/component/ProductCard/ProductCard.container.js
@@ -148,15 +148,15 @@ export class ProductCardContainer extends PureComponent {
     _getProductOrVariant() {
         const { product: { type_id, variants }, product } = this.props;
 
-        return (
-            (
-                type_id === 'configurable'
-                && variants !== undefined
-                && variants.length
-            )
-                ? variants[this._getCurrentVariantIndex()]
-                : product
-        ) || {};
+        if (
+            type_id === 'configurable'
+            && variants !== undefined
+            && variants.length
+        ) {
+            return variants[this._getCurrentVariantIndex()] || {};
+        }
+
+        return product || {};
     }
 
     _getAvailableVisualOptions() {
