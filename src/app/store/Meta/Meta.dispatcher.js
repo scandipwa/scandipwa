c05cd06b800d182fa9899b190be46f74343c64d1
ejnshtein <ejnshtein@gmail.com>
2020-09-28 16:45:49 +0300

    fix double slash for canonical tags

--- src/app/store/Meta/Meta.dispatcher.js
+++ src/app/store/Meta/Meta.dispatcher.js
@@ -58,7 +58,7 @@ export class MetaDispatcher {
             description: meta_description,
             keywords: meta_keyword,
             title: meta_title || name,
-            canonical_url: `${window.location.origin}/${appendWithStoreCode(canonical_url)}`
+            canonical_url: `${window.location.origin}${appendWithStoreCode(canonical_url)}`
         };
     }
 
@@ -78,7 +78,7 @@ export class MetaDispatcher {
             description: meta_description || description,
             title: meta_title || name,
             keywords: meta_keyword,
-            canonical_url: `${window.location.origin}/${appendWithStoreCode(canonical_url)}`
+            canonical_url: `${window.location.origin}${appendWithStoreCode(canonical_url)}`
         };
     }
 }
