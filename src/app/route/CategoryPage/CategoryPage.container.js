8cd01da6228938e6ed7f091b8be8f98e50d974bb
ejnshtein <ejnshtein@gmail.com>
2020-09-30 10:38:20 +0300

    update robots meta tag of filter change

--- src/app/route/CategoryPage/CategoryPage.container.js
+++ src/app/route/CategoryPage/CategoryPage.container.js
@@ -221,7 +221,10 @@ export class CategoryPageContainer extends PureComponent {
             categoryIds,
             category: {
                 id
-            }
+            },
+            currentArgs: {
+                filter
+            } = {}
         } = this.props;
 
         const {
@@ -232,7 +235,10 @@ export class CategoryPageContainer extends PureComponent {
             categoryIds: prevCategoryIds,
             category: {
                 id: prevId
-            }
+            },
+            currentArgs: {
+                filter: prevFilter
+            } = {}
         } = prevProps;
 
         // TODO: category scrolls up when coming from PDP
@@ -264,14 +270,24 @@ export class CategoryPageContainer extends PureComponent {
          * Or if the breadcrumbs were not yet updated after category request,
          * and the category ID expected to load was loaded, update data.
          */
-        if (
-            id !== prevId
-            || (!breadcrumbsWereUpdated && id === categoryIds)
-        ) {
+        const categoryChange = id !== prevId || (!breadcrumbsWereUpdated && id === categoryIds);
+
+        if (categoryChange) {
             this.checkIsActive();
             this.updateMeta();
             this.updateBreadcrumbs();
             this.updateHeaderState();
+        }
+
+        /*
+        ** if category wasn't changed we still need to update meta for correct robots meta tag [#928](https://github.com/scandipwa/base-theme/issues/928)
+        */
+        if (!categoryChange
+            && filter
+            && prevFilter
+            && Object.keys(filter.customFilters).length !== Object.keys(prevFilter.customFilters).length
+        ) {
+            this.updateMeta();
         }
     }
 
