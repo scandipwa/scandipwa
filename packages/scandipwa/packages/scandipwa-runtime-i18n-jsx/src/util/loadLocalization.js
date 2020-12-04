af5b1e4b33f4c7a951f2c8be09fb39a1aa0ad7df
yeegor <yegor.batov@gmail.com>
2020-12-04 17:19:44 +0200

    Implement JSX translator - not usable when plain text is required

---
+++ packages/scandipwa/packages/scandipwa-runtime-i18n-jsx/src/util/loadLocalization.js
@@ -0,0 +1,43 @@
+import mergeTranslations from './mergeTranslations';
+
+/**
+ * Entry format:
+ * locale: [
+ *   child translation,
+ *   parent translation,
+ *   grandparent translation,
+ *   ...
+ *   extensions' translations
+ * ]
+ */
+function loadTranslationFiles(locale) {
+    switch(locale) {
+    case 'en_US':
+        return [
+            import(/* webpackMode: "lazy", webpackChunkName: "en_US" */'../../../../i18n/en_US.json')
+        ]
+    case 'ru_RU':
+        return [
+            import(/* webpackMode: "lazy", webpackChunkName: "ru_RU" */'../../../../i18n/ru_RU.json')
+        ]
+    case 'lv_LV':
+        return [
+            import(/* webpackMode: "lazy", webpackChunkName: "lv_LV" */'../../../../i18n/lv_LV.json')
+        ]
+    case 'fr_FR':
+        return [
+            import(/* webpackMode: "lazy", webpackChunkName: "fr_FR" */'../../../../i18n/fr_FR.json')
+        ]
+
+    default:
+        throw new Error('Such locale is not provided!');
+    }
+}
+
+// Strategy pattern ensures proper chunk splitting
+export default async function loadLocalization(locale) {
+    // Import the translations
+    const translations = await Promise.all(loadTranslationFiles(locale));
+
+    return mergeTranslations(translations);
+}
