1489a8be46dc07982be2d33e6c9b6c9518c6597b
winniepukki <4bogaa25@solent.ac.uk>
2020-12-28 16:33:42 +0200

    1907-change-price-color-issue-fix

--- packages/scandipwa/src/component/ProductPrice/ProductPrice.component.js
+++ packages/scandipwa/src/component/ProductPrice/ProductPrice.component.js
@@ -91,6 +91,11 @@ export class ProductPrice extends PureComponent {
         );
     }
 
+    isDiscountApplied() {
+        const { discountPercentage } = this.props;
+        return !discountPercentage;
+    }
+
     renderOldPrice() {
         const {
             roundedRegularPrice,
@@ -151,6 +156,7 @@ export class ProductPrice extends PureComponent {
               block="ProductPrice"
               mix={ mix }
               aria-label={ `Product price: ${formattedFinalPrice}` }
+              style={ { color: this.isDiscountApplied() ? 'black' : '' } }
             >
                 { this.renderCurrentPrice() }
                 { this.renderOldPrice() }
