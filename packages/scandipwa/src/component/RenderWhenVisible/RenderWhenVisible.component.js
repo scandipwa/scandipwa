fa631af618f162a410c082e4664176df049c8b7d
Zhan Lyaksa <zans.laksa@scandiweb.com>
2021-03-03 20:11:57 +0200

    See #1929 Fixed CR requested changes

--- packages/scandipwa/src/component/RenderWhenVisible/RenderWhenVisible.component.js
+++ packages/scandipwa/src/component/RenderWhenVisible/RenderWhenVisible.component.js
@@ -13,6 +13,7 @@ import { PureComponent } from 'react';
 import { InView } from 'react-intersection-observer';
 
 import { ChildrenType } from 'Type/Common';
+import { isCrawler, isSSR } from 'Util/Browser';
 
 import './RenderWhenVisible.style';
 
@@ -53,7 +54,7 @@ export class RenderWhenVisible extends PureComponent {
 
     shouldRender() {
         const { wasVisible } = this.state;
-        return !wasVisible && !this.isSSR() && !this.isCrawler();
+        return !wasVisible && !isSSR() && !isCrawler();
     }
 
     handleVisibilityToggle = (isVisible) => {
@@ -63,18 +64,6 @@ export class RenderWhenVisible extends PureComponent {
             this.setState({ wasVisible: true });
         }
     };
-
-    isSSR() {
-        return !globalThis.window || !globalThis.window.document;
-    }
-
-    isCrawler() {
-        const { userAgent } = navigator;
-        // eslint-disable-next-line max-len
-        const re = /(googlebot\/|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis|HeadlessChrome)/i;
-
-        return re.test(userAgent);
-    }
 
     renderFallback() {
         const { fallback } = this.props;
