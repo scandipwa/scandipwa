2104aec57c123afd9c29c830b41f32a553dbcf9b
Rihards Abolins <rihards.abolins@scandiweb.com>
2021-03-12 09:54:31 +0200

    #2269 Downloadable type

--- packages/scandipwa/src/util/Product/Types.js
+++ packages/scandipwa/src/util/Product/Types.js
@@ -12,4 +12,4 @@ export const CONFIGURABLE = 'configurable';
 export const GROUPED = 'grouped';
 export const SIMPLE = 'simple';
 export const BUNDLE = 'bundle';
-export const DOWNLOADABLE = 'downloadable'
+export const DOWNLOADABLE = 'downloadable';
