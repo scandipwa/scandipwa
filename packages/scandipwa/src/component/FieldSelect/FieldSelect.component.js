4c5a1041c56bfb02f866332123a296f7ee1120d3
hung.nguyen <linqtojs@gmail.com>
2021-04-01 09:55:53 +0700

    fix #2247

--- packages/scandipwa/src/component/FieldSelect/FieldSelect.component.js
+++ packages/scandipwa/src/component/FieldSelect/FieldSelect.component.js
@@ -90,7 +90,6 @@ export class FieldSelect extends PureComponent {
               name={ name }
               id={ id }
               disabled={ isDisabled }
-              tabIndex="0"
               value={ value || '' }
               onChange={ onChange }
               data-skip-value={ skipValue }
@@ -196,12 +195,12 @@ export class FieldSelect extends PureComponent {
 
         return (
             <ClickOutside onClick={ handleSelectExpandedExpand }>
+                { /* eslint-disable-next-line jsx-a11y/interactive-supports-focus */ }
                 <div
                   block="FieldSelect"
                   onClick={ handleSelectExpand }
                   onKeyPress={ handleSelectListKeyPress }
                   role="button"
-                  tabIndex="0"
                   aria-label="Select drop-down"
                   aria-expanded={ isExpanded }
                 >
