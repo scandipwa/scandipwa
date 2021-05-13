a6b81bc326294b6504b2aa05eeada5b79f510d26
tappiola <tatiana.karamorina@scandiweb.com>
2021-05-13 22:14:42 +0300

    Improve styling

--- packages/scandipwa/src/route/Checkout/Checkout.component.js
+++ packages/scandipwa/src/route/Checkout/Checkout.component.js
@@ -158,18 +158,18 @@ export class Checkout extends PureComponent {
         }
 
         return (
-            <>
-            <div block="Checkout" elem="Header">
-                <div block="Checkout" elem="Title">{ title }</div>
-                <div block="Checkout" elem="Step">
-                    <span block="Checkout" elem="SelectedStep">{ number }</span>
-                    <span block="Checkout" elem="StepsBorder">/</span>
-                    <span block="Checkout" elem="TotalSteps">{ this.stepsCount }</span>
+            <div block="Checkout" elem="ProgressSection">
+                <div block="Checkout" elem="Header">
+                    <div block="Checkout" elem="Title">{ title }</div>
+                    <div block="Checkout" elem="Step">
+                        <span block="Checkout" elem="SelectedStep">{ number }</span>
+                        <span block="Checkout" elem="StepsBorder">/</span>
+                        <span block="Checkout" elem="TotalSteps">{ this.stepsCount }</span>
+                    </div>
                 </div>
-            </div>
                 <div block="Checkout" elem="StepBarTotal" />
                 <div block="Checkout" elem="StepBarActive" mods={ { isSecond: number === 2 } } />
-            </>
+            </div>
         );
     }
 
