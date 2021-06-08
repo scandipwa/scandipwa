53fb9710f13874731f9560ab9e79cde6a47b21b6
Zans Laksa <zans.laksa@scandiweb.com>
2021-06-08 19:19:09 +0300

    See #2271 Fixed some console errors

--- packages/scandipwa/src/component/MyAccountOrderPopup/MyAccountOrderPopup.container.js
+++ packages/scandipwa/src/component/MyAccountOrderPopup/MyAccountOrderPopup.container.js
@@ -32,7 +32,7 @@ export const OrderDispatcher = import(
 export const mapStateToProps = (state) => ({
     order: state.OrderReducer.order,
     payload: state.PopupReducer.popupPayload[ORDER_POPUP_ID] || {},
-    display_tax_in_shipping_amount: state.ConfigReducer.cartDisplayConfig.display_tax_in_shipping_amount
+    display_tax_in_shipping_amount: state.ConfigReducer.cartDisplayConfig.display_tax_in_shipping_amount || ''
 });
 
 /** @namespace Component/MyAccountOrderPopup/Container/mapDispatchToProps */
@@ -51,7 +51,8 @@ export class MyAccountOrderPopupContainer extends PureComponent {
             increment_id: PropTypes.string
         }).isRequired,
         showNotification: PropTypes.func.isRequired,
-        getOrder: PropTypes.func.isRequired
+        getOrder: PropTypes.func.isRequired,
+        display_tax_in_shipping_amount: PropTypes.string.isRequired
     };
 
     state = {
@@ -83,9 +84,13 @@ export class MyAccountOrderPopupContainer extends PureComponent {
 
     containerProps = () => {
         const { order: stateOrder, isLoading, currency_code } = this.state;
-        const { payload: { order: payloadOrder } } = this.props;
+        const {
+            payload: { order: payloadOrder },
+            display_tax_in_shipping_amount
+        } = this.props;
 
         return {
+            display_tax_in_shipping_amount,
             isLoading,
             currency_code,
             order: {
