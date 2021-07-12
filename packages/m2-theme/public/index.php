9ae04c76869b8a7a38fcb8e5cb60bd1a72769008
tappiola <tatiana.karamorina@scandiweb.com>
2021-07-12 12:29:49 +0100

    change ltr to rtl for testing

--- packages/m2-theme/public/index.php
+++ packages/m2-theme/public/index.php
@@ -4,7 +4,7 @@
     $icons = $this->getAppIconData();
 ?>
 <!DOCTYPE html>
-<html lang="<?= $this->getLanguageCode() ?>" dir="ltr">
+<html lang="<?= $this->getLanguageCode() ?>" dir="rtl">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover">
