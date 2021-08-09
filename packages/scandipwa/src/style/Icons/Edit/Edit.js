1a871eff1f18e3814d8f5ae7cc79e0d69fc5cff8
Aleksandrs <aleksandrs.kondratjevs@scandiweb.com>
2021-08-09 13:41:27 +0300

    Wrapp edit icon into button

--- packages/scandipwa/src/style/Icons/Edit/Edit.js
+++ packages/scandipwa/src/style/Icons/Edit/Edit.js
@@ -10,22 +10,11 @@
  */
 import './Edit.scss';
 
-import PropTypes from 'prop-types';
 import { PureComponent } from 'react';
 
 /** @namespace Component/EditIcon/Component */
 export class EditIcon extends PureComponent {
-    static propTypes = {
-        onClick: PropTypes.func
-    };
-
-    static defaultProps = {
-        onClick: () => {}
-    };
-
     render() {
-        const { onClick } = this.props;
-
         return (
             <svg
               block="EditIcon"
@@ -34,7 +23,6 @@ export class EditIcon extends PureComponent {
               viewBox="0 0 16 20"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              onClick={ onClick }
             >
                 <path d="M15.045 5.401C15.423 5.023 15.631 4.521 15.631 3.987C15.631 3.453 15.423 2.951 15.045 2.573L13.459 0.987001C13.081 0.609001 12.579 0.401001 12.045 0.401001C11.511 0.401001 11.009 0.609001 10.632 0.986001L0 11.585V16H4.413L15.045 5.401ZM12.045 2.401L13.632 3.986L12.042 5.57L10.456 3.985L12.045 2.401ZM2 14V12.415L9.04 5.397L10.626 6.983L3.587 14H2ZM0 18H16V20H0V18Z" />
             </svg>
