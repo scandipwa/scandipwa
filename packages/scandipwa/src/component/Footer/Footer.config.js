eaebd6a786d0609bacd2f2698c5c5ba89c821a4f
tappiola <tatiana.karamorina@scandiweb.com>
2021-08-16 10:52:24 +0100

    fix scroll

--- packages/scandipwa/src/component/Footer/Footer.config.js
+++ packages/scandipwa/src/component/Footer/Footer.config.js
@@ -9,9 +9,9 @@
  * @link https://github.com/scandipwa/base-theme
  */
 
-import facebookIcon from 'Style/Icons/Logos/facebook.svg';
-import linkedinIcon from 'Style/Icons/Logos/linkedIn.svg';
-import twitterIcon from 'Style/Icons/Logos/twitter.svg';
+import facebookIcon from 'Style/icons/logos/facebook.svg';
+import linkedinIcon from 'Style/icons/logos/linkedIn.svg';
+import twitterIcon from 'Style/icons/logos/twitter.svg';
 
 export const RENDER_NEWSLETTER = 'render_newsletter';
 
