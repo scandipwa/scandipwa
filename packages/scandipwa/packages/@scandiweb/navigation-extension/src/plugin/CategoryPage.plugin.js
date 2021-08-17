13c2fbd3b667ec3b19d547655d61127eb0aba003
tappiola <tatiana.karamorina@scandiweb.com>
2021-08-17 20:41:00 +0100

    Disable extension styles overrides

--- packages/scandipwa/packages/@scandiweb/navigation-extension/src/plugin/CategoryPage.plugin.js
+++ packages/scandipwa/packages/@scandiweb/navigation-extension/src/plugin/CategoryPage.plugin.js
@@ -10,7 +10,7 @@ import { isCrawler, isSSR } from 'Util/Browser';
  * @package scandipwa/base-theme
  * @link https://github.com/scandipwa/base-theme
  */
-import './CategoryPage.style.plugin.scss';
+// import './CategoryPage.style.plugin.scss';
 
 export const renderMiscellaneous = (args, callback, instance) => {
     const { category: { name } = {}, isMobile } = instance.props;
