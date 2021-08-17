a0a7057c99ce722213dc484201830016ceb49430
tappiola <tatiana.karamorina@scandiweb.com>
2021-08-17 22:25:28 +0100

    Partially enable header and product actions styles

--- packages/scandipwa/packages/@scandiweb/navigation-extension/src/plugin/Header.plugin.js
+++ packages/scandipwa/packages/@scandiweb/navigation-extension/src/plugin/Header.plugin.js
@@ -18,9 +18,9 @@ import SearchIcon from 'Component/SearchIcon';
 import UserIcon from 'Component/UserIcon';
 import { isSignedIn } from 'Util/Auth';
 
-import './Header.style.plugin';
-import './Footer.style.plugin';
-// import './CheckoutPage.style.plugin.scss';
+import './Header.style.plugin.scss';
+import './Footer.style.plugin.scss';
+import './CheckoutPage.style.plugin.scss';
 
 export const HamburgerMenu = lazy(() => import(
     /* webpackMode: "lazy", webpackChunkName: "navigation-extension" */
