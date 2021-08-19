5588a0a27ff2ebece227fec875ef62bdfd762017
tappiola <tatiana.karamorina@scandiweb.com>
2021-08-19 17:21:56 +0100

    Hide bottom navigation bar

---
+++ packages/scandipwa/packages/@scandiweb/navigation-extension/src/plugin/SearchItem.container.plugin.js
@@ -0,0 +1,24 @@
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/base-theme
+ * @link https://github.com/scandipwa/base-theme
+ */
+export const handleItemClick = (args, callback, instance) => {
+    const { hideActiveOverlay, deactivateSearchBar } = instance.props;
+
+    hideActiveOverlay();
+    deactivateSearchBar();
+};
+
+export default {
+    'Component/SearchItem/Container': {
+        'member-function': {
+            handleItemClick
+        }
+    }
+};
---
+++ packages/scandipwa/packages/@scandiweb/navigation-extension/src/plugin/SearchItem.dispatch.plugin.js
@@ -0,0 +1,32 @@
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/base-theme
+ * @link https://github.com/scandipwa/base-theme
+ */
+
+const MobileSearchBarDispatcher = import(
+    /* webpackMode: "lazy", webpackChunkName: "dispatchers" */
+    '../store/MobileSearchBar/MobileSearchBar.dispatcher'
+);
+
+const mapDispatchToProps = (args, callback) => {
+    const [dispatch] = args;
+
+    return {
+        ...callback(...args),
+        deactivateSearchBar: () => MobileSearchBarDispatcher.then(
+            ({ default: dispatcher }) => dispatcher.deactivateSearchBar(dispatch)
+        )
+    };
+};
+
+export default {
+    'Component/SearchItem/Container/mapDispatchToProps': {
+        function: mapDispatchToProps
+    }
+};
