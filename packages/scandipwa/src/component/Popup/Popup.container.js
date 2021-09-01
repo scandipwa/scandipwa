3e736c2b78ae9252823e4487bd780ff4569537fd
tappiola <tatiana.karamorina@scandiweb.com>
2021-09-01 16:51:28 +0100

    #3248 - no go back after popup close

--- packages/scandipwa/src/component/Popup/Popup.container.js
+++ packages/scandipwa/src/component/Popup/Popup.container.js
@@ -54,17 +54,20 @@ export class PopupContainer extends PureComponent {
         changeHeaderState: PropTypes.func.isRequired,
         onVisible: PropTypes.func,
         onClose: PropTypes.func,
+        onHide: PropTypes.func,
         isStatic: PropTypes.bool,
         children: ChildrenType,
         id: PropTypes.string.isRequired,
         shouldPopupClose: PropTypes.bool.isRequired,
         isMobile: PropTypes.bool.isRequired,
-        hideActiveOverlay: PropTypes.func.isRequired
+        hideActiveOverlay: PropTypes.func.isRequired,
+        resetHideActivePopup: PropTypes.func.isRequired
     };
 
     static defaultProps = {
         onVisible: () => {},
         onClose: () => {},
+        onHide: () => {},
         mix: {},
         contentMix: {},
         children: [],
@@ -78,10 +81,13 @@ export class PopupContainer extends PureComponent {
     onVisible() {
         const { changeHeaderState, onVisible } = this.props;
 
+        console.debug('trigger on visible');
+
         changeHeaderState({
             name: POPUP,
             title: this._getPopupTitle(),
             onCloseClick: () => {
+                console.debug('onCloseClick');
                 history.back();
             }
         });
@@ -101,8 +107,11 @@ export class PopupContainer extends PureComponent {
             mix,
             contentMix,
             onClose,
+            onHide,
             onVisible,
+            shouldPopupClose,
             hideActiveOverlay,
+            resetHideActivePopup,
             goToPreviousNavigationState
         } = this.props;
 
@@ -116,9 +125,12 @@ export class PopupContainer extends PureComponent {
             isStatic,
             mix,
             contentMix,
+            shouldPopupClose,
             onClose,
+            onHide,
             onVisible,
             hideActiveOverlay,
+            resetHideActivePopup,
             goToPreviousNavigationState,
             title: this._getPopupTitle()
         };
