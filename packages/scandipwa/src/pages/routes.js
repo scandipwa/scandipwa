70787964508f0fe544f99ebc7e785df48a539cab
Zans Laksa <zans.laksa@scandiweb.com>
2021-09-09 13:38:57 +0300

    Removed routes

--- packages/scandipwa/src/pages/routes.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-import * as products_index from './products/index';
-import * as products__id_ from './products/[id]';
-
-export default {
-    products_index,
-    products__id_
-};
