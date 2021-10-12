966288ccde5f0f334818a3c74a530c67ac57dd53
Aleksandrs <aleksandrs.kondratjevs@scandiweb.com>
2021-10-12 03:56:26 +0300

    Fetch delivery methods on first checkout visit

--- packages/scandipwa/src/component/CheckoutAddressForm/CheckoutAddressForm.component.js
+++ packages/scandipwa/src/component/CheckoutAddressForm/CheckoutAddressForm.component.js
@@ -26,6 +26,28 @@ export class CheckoutAddressForm extends MyAccountAddressForm {
         onShippingEstimationFieldsChange: () => {}
     };
 
+    componentDidMount() {
+        const {
+            address: {
+                countryId,
+                regionId,
+                region,
+                city,
+                postcode
+            },
+            defaultCountry,
+            onShippingEstimationFieldsChange
+        } = this.props;
+
+        onShippingEstimationFieldsChange({
+            country_id: countryId || defaultCountry,
+            region_id: regionId,
+            region,
+            city,
+            postcode
+        });
+    }
+
     get fieldMap() {
         const fieldMap = super.fieldMap;
         const addressGroup = fieldMap.find(({ name }) => name === 'addressGroup');
@@ -38,6 +60,7 @@ export class CheckoutAddressForm extends MyAccountAddressForm {
                 onLoad: this.onAddressChange
             };
         }
+
         fieldMap.splice(0, 2);
 
         return fieldMap;
