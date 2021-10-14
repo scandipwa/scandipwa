6178af890aaf760e1cb9c01ffeb948ba10e4e53e
Okabe Rintaro <ejnshtein@gmail.com>
2021-10-14 10:48:34 +0300

    adjust global types

--- packages/scandipwa/src/type/OverrideGlobals.d.ts
+++ packages/scandipwa/src/type/OverrideGlobals.d.ts
@@ -1,6 +1,8 @@
 import { DataType } from '@tilework/opus';
 import { StoreEnhancer } from 'redux';
 
+import { ValidationData } from 'Util/Validator';
+
 declare global {
     interface Window {
         '__REACT_DEVTOOLS_GLOBAL_HOOK__'?: Record<string, unknown>
@@ -10,10 +12,10 @@ declare global {
             type?: string
         }
         secure_base_media_url?: string
-        dataCache?: Record<number, DataType<any>>
+        // dataCache?: Record<number, DataType<any>>
     }
 
-    function __(message: string): string
+    function __(message: string, ...args: any[]): string
 
     namespace React {
 
@@ -25,6 +27,11 @@ declare global {
             block?: string
             elem?: string
             mods?: Record<string, unknown>
+            mix?: Record<string, unknown>
         }
+    }
+
+    interface HTMLElementEventMap {
+        validate: ValidationData
     }
 }
