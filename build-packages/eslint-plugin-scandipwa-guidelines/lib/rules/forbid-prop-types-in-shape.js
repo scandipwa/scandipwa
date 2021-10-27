a27b7c6b8e025b3a5a4dd210e12e2487603e0106
tappiola <tatiana.karamorina@scandiweb.com>
2021-10-27 11:05:57 +0300

    prop types refactoring

--- build-packages/eslint-plugin-scandipwa-guidelines/lib/rules/forbid-prop-types-in-shape.js
+++ build-packages/eslint-plugin-scandipwa-guidelines/lib/rules/forbid-prop-types-in-shape.js
@@ -1,37 +1,38 @@
 /**
- * @fileoverview Forbid certain prop types to be used in PropTypes.shape
+ * @fileoverview Forbid usage of empty PropTypes.shape
  * @author Tatiana Karamorina
  */
 
 module.exports = {
     meta: {
         docs: {
-            description: 'Forbid usage of PropTypes.shape with empty content.',
+            description: 'Forbid usage of empty PropTypes.shape.',
             category: 'Coding standard',
             recommended: true
         }
     },
     create: (context) => ({
         MemberExpression(node) {
-            function isShapePropType(node){
+            function isShapePropType(node) {
                 return node.object.name === 'PropTypes' && node.property.name === 'shape';
             }
 
-            if(isShapePropType(node) && node.parent.arguments === undefined) {
-                context.report({
-                    node: node.property,
-                    message: `Found empty PropTypes.shape`
-                });
-            }
+            if (isShapePropType(node)) {
+                // forbid `PropTypes.shape`
+                if (node.parent.arguments === undefined) {
+                    context.report({
+                        node: node.property,
+                        message: `Found empty PropTypes.shape`
+                    });
+                }
 
-            if(isShapePropType(node)
-                && node.parent.arguments?.length === 1
-                && node.parent.arguments[0].properties?.length === 0
-            ) {
-                context.report({
-                    node: node.property,
-                    message: `Found PropTypes.shape with empty object as parameter`
-                });
+                // forbid `PropTypes.shape({})`
+                if (node.parent.arguments?.length === 1 && node.parent.arguments[0].properties?.length === 0) {
+                    context.report({
+                        node: node.property,
+                        message: `Found PropTypes.shape with empty object as parameter`
+                    });
+                }
             }
         }
     })
--- packages/framework/src/component/App/App.component.js
+++ packages/framework/src/component/App/App.component.js
@@ -1,3 +1,4 @@
+import { ErrorDetailsType } from '@scandipwa/scandipwa/src/type/Error.type';
 import PropTypes from 'prop-types';
 import { PureComponent } from 'react';
 
@@ -6,8 +7,7 @@ import SomethingWentWrong from '../SomethingWentWrong/SomethingWentWrong.compone
 /** @namespace Framework/Component/App/Component */
 export class AppComponent extends PureComponent {
     static propTypes = {
-        // eslint-disable-next-line react/forbid-prop-types
-        errorDetails: PropTypes.object.isRequired,
+        errorDetails: ErrorDetailsType.isRequired,
         isSomethingWentWrong: PropTypes.bool.isRequired,
         handleErrorReset: PropTypes.func.isRequired
     };
