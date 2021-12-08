e6984159c24db6548c3711b010dff484145ecd70
Aleksandrs <aleksandrs.kondratjevs@scandiweb.com>
2021-12-08 17:33:57 +0200

    Add fallback for cacheTTL in case if it is not intereger

--- packages/scandipwa/src/util/Request/Request.js
+++ packages/scandipwa/src/util/Request/Request.js
@@ -13,7 +13,7 @@
 
 import { getAuthorizationToken } from 'Util/Auth';
 import { getCurrency } from 'Util/Currency';
-
+import { ONE_MONTH_IN_SECONDS } from 'Util/Request/QueryDispatcher';
 import { hash } from './Hash';
 
 export const GRAPHQL_URI = '/graphql';
@@ -114,7 +114,7 @@ export const putPersistedQuery = (graphQlURI, query, cacheTTL) => fetch(`${ grap
         body: JSON.stringify(query),
         headers: {
             'Content-Type': 'application/json',
-            'SW-Cache-Age': cacheTTL
+            'SW-Cache-Age': Number.isInteger(cacheTTL) ? cacheTTL : ONE_MONTH_IN_SECONDS
         }
     });
 
