904db5401aee31ff41ac7f7044039504931c8d03
Iman Dodo Sujatmiko <iman.sujamiko@scandiweb.com>
2022-01-13 21:10:16 +0700

    disable redirectIfNotOnMobile in componentDidMount()

--- packages/scandipwa/src/route/MenuPage/MenuPage.container.js
+++ packages/scandipwa/src/route/MenuPage/MenuPage.container.js
@@ -45,11 +45,15 @@ export class MenuPageContainer extends PureComponent {
     componentDidMount() {
         const { updateMeta, changeHeaderState } = this.props;
         updateMeta({ title: __('Menu') });
-        this.redirectIfNotOnMobile();
+        // this.redirectIfNotOnMobile();
         changeHeaderState({
             name: MENU,
             onBackClick: () => history.goBack()
         });
+    }
+
+    componentDidUpdate() {
+        this.redirectIfNotOnMobile();
     }
 
     redirectIfNotOnMobile() {
