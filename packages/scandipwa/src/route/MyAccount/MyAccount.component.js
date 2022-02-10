eec1da7d28c7d2824c5f55d2221da79c29d06868
pablobentivengo <pablo.bentivengo@scandiweb.com>
2022-02-10 09:30:39 +0100

    #4245 Added styling to Loader over my account tabs to render in center

--- packages/scandipwa/src/route/MyAccount/MyAccount.component.js
+++ packages/scandipwa/src/route/MyAccount/MyAccount.component.js
@@ -189,7 +189,7 @@ export class MyAccount extends Component {
                         { title || tabName }
                         { this.renderSubHeading() }
                     </h2>
-                    <Suspense fallback={ <Loader /> }>
+                    <Suspense fallback={ <Loader mix={ { block: 'MyAccount', elem: 'Loader' } } /> }>
                         <TabContent
                           isEditingActive={ isEditingActive }
                           match={ match }
