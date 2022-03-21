2a9fd27bea8c27ca53178a7f58e6b6f2e42626fb
florianf22 <g.fasieshvili@gmail.com>
2022-03-21 19:12:11 +0400

    fixed radio buttons not selecting correctly

--- packages/scandipwa/src/component/Product/Product.container.js
+++ packages/scandipwa/src/component/Product/Product.container.js
@@ -271,6 +271,7 @@ export class ProductContainer extends PureComponent {
         };
     }
 
+    // TODO: Radio buttons weren't behaving correctly due to e.preventDefault();
     /**
      * Fetches form data for customizable and bundle options.
      * (Should be called when value is changed)
