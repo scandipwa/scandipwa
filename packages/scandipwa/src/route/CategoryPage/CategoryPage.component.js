7f44e92c4a11f6b48690b20c7aa3e2ae93db671b
Levan Apkhazava <levan.apkhazava@scandiweb.com>
2022-03-24 14:17:17 +0400

    #4375 - added a Shopping Options heading to the CategoryPage filter placeholder

--- packages/scandipwa/src/route/CategoryPage/CategoryPage.component.js
+++ packages/scandipwa/src/route/CategoryPage/CategoryPage.component.js
@@ -229,8 +229,13 @@ export class CategoryPage extends PureComponent {
 
     renderFilterPlaceholder() {
         return (
-            <div block="CategoryPage" elem="FilterPlaceholderContainer">
-                { this.renderPlaceholder('CategoryPage') }
+            <div block="CategoryPage" elem="PlaceholderWrapper">
+                <h3 block="CategoryPage" elem="PlaceholderHeading">
+                    { __('Shopping Options') }
+                </h3>
+                <div block="CategoryPage" elem="FilterPlaceholderContainer">
+                    { this.renderPlaceholder('CategoryPage') }
+                </div>
             </div>
         );
     }
