fcf5a26127b34c76a4327c2eacb70f3ab00840d7
Zans Laksa <zans.laksa@scandiweb.com>
2022-04-01 03:51:58 +0300

    Migrated some queries

--- packages/scandipwa/src/query/Query.type.ts
+++ packages/scandipwa/src/query/Query.type.ts
@@ -9,6 +9,8 @@
  * @link https://github.com/scandipwa/scandipwa
  */
 
+import { GQLCustomerInput } from 'Type/Graphql.type';
+
 export type CategoryQueryOptions = {
     categoryIds: number;
 };
@@ -17,4 +19,48 @@ export type CmsPage = {
     id: number;
     url_key: string;
     identifier: string;
+};
+
+export type ResetPasswordOptions = {
+    token: string;
+    password: string;
+    password_confirmation: string;
+};
+
+export type SignInOptions = {
+    email: string;
+    password: string;
+};
+
+export type ConfirmAccountOptions = SignInOptions & {
+    key: string;
+};
+
+export type ChangeCustomerPasswordOptions = {
+    password: string;
+    newPassword: string;
+};
+
+export type CreateAccountOptions = {
+    customer: Omit<GQLCustomerInput, 'password'>;
+    password: string;
+};
+
+export type OrdersOptions = {
+    orderId: number;
+    page: number;
+};
+
+export type ProductListOptions = {
+    isSingleProduct: boolean;
+    isPlp: boolean;
+    isForWishlist: boolean;
+    isForLinkedProducts: boolean;
+    noAttributes: boolean;
+    noVariants: boolean;
+    noVariantAttributes: boolean;
+    requireInfo: boolean;
+    notRequireInfo: boolean;
+    categoryIds: number[];
+    args: Record<string, unknown>;
 };
