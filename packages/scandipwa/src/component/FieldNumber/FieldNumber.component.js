74a83f71a72881deac25e5eb9622fc927af7762c
Azizbek Rasulov <azizbek.rasulov@scandiweb.com>
2022-04-19 18:33:58 +0500

    value is taken from attr in fieldNumber

--- packages/scandipwa/src/component/FieldNumber/FieldNumber.component.js
+++ packages/scandipwa/src/component/FieldNumber/FieldNumber.component.js
@@ -43,7 +43,7 @@ export class FieldNumber extends PureComponent {
             isDisabled
         } = this.props;
 
-        const numberValue = +value;
+        const numberValue = +attr.value || +value;
 
         return (
             <>
@@ -56,7 +56,7 @@ export class FieldNumber extends PureComponent {
                   type={ FIELD_TYPE.number }
                   readOnly
                   aria-label={ __('Value') }
-                  value={ value }
+                  value={ attr.value || value }
                   disabled={ isDisabled }
                 />
                 <button
