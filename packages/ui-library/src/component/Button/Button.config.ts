c11f7b460662cee7306d58e97386e6d859e9483c
Anastasija Zubkova <anastasijazubkova@Anastasijas-MacBook-Air.local>
2022-11-23 15:45:18 +0200

    UI Library Button WIP

--- packages/ui-library/src/component/Button/Button.config.ts
+++
@@ -1,4 +0,0 @@
-export const BUTTON_TYPE = {
-    BUTTON: 'button',
-    SUBMIT: 'submit'
-};
--- packages/ui-library/src/component/Checkbox/Checkbox.type.ts
+++
@@ -1,7 +0,0 @@
-export interface CheckboxComponentProps {
-    label: string
-    id: string
-    isDisabled: boolean
-    onCheckedChange: any
-    checked: boolean
-}
