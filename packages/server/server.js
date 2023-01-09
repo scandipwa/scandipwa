8080a6d8d6c6f5d7880ce031c51ee67ce3499270
aleksandrskondratjevs <aleksandrs.kondratjevs@scandiweb.com>
2023-01-09 11:46:11 +0200

    Add server for development

---
+++ packages/server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const compression = require('compression');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const fs = require('fs');
+const path = require('path');
+const spdy = require('spdy');
+
+const app = express();
+
+app.use(compression());
+app.use(express.static('../scandipwa/build'));
+app.use('/tech_storeview/graphql', createProxyMiddleware({
+    target: 'https://tech-demo.scandipwa.com//',
+    changeOrigin: true,
+}));
+
+app.get('/tech_storeview/laptops-and-computers.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../scandipwa/build/index.html'));
+});
+
+app.get('/tech_storeview/product-74538.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../scandipwa/build/index.html'));
+});
+
+const options = {
+    key: fs.readFileSync(path.join(__dirname, '/privateKey.key')),
+    cert: fs.readFileSync(path.join(__dirname, '/certificate.crt')),
+};
+
+spdy.createServer(options, app).listen(8080, (error) => {
+    if (error) {
+        console.error(error);
+
+        return process.exit(1);
+    }
+    console.log('HTTP/2 server listening on port: 8080');
+});
