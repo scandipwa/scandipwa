355bd7058693891be2454e741a8800eaba37572b
Mate Sitchinava <mate.sitchinava@scandiweb.com>
2023-01-10 20:34:18 +0400

    added PLP and PDP fallbacks

---
+++ packages/scandipwa/src/component/Fallback/Fallback.type.ts
@@ -0,0 +1,15 @@
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/scandipwa
+ * @link https://github.com/scandipwa/scandipwa
+ */
+import { UrlRewritePageType } from 'Route/UrlRewrites/UrlRewrites.config';
+
+export interface FallbackComponentProps {
+    type: UrlRewritePageType;
+}
