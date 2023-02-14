1826a8227e300d18afea20a0150becec6e0cebed
aleksandrskondratjevs <aleksandrs.kondratjevs@scandiweb.com>
2023-02-14 10:17:27 +0200

    Add preact debug library for dev mode

--- packages/chunk-optimizer/build-config/craco.js
+++ packages/chunk-optimizer/build-config/craco.js
@@ -12,6 +12,12 @@ module.exports = {
             webpackConfig.resolve.alias.react = 'preact/compat';
             webpackConfig.resolve.alias['react-dom'] = 'preact/compat';
 
+            // For development mode add preact debug library.
+            if (process.env.NODE_ENV === 'development') {
+                const { entry } = webpackConfig;
+                webpackConfig.entry = ['preact/debug'].concat(entry);
+            }
+
             webpackConfig.plugins.forEach((plugin) => {
                 if (plugin.definitions?.PureComponent) {
                     // plugin.definitions.PureComponent[0] = 'preact/compat';
