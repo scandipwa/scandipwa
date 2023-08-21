8b8b665882b022844060059947c6c53fc1072784
Andrii Antoniuk <andrewantonuik@gmail.com>
2023-08-21 15:51:02 +0200

    CPWA-7 * Implemented command for creation of workspace

--- build-packages/scandipwa-cli/actions/generate/index.js
+++ build-packages/scandipwa-cli/actions/generate/index.js
@@ -1,7 +1,12 @@
-const { createExtension, installExtension, createWorkspace } = require('@scandipwa/scandipwa-development-toolkit-core');
+const {
+    createWorkspace,
+} = require('@scandipwa/scandipwa-development-toolkit-core');
 const logger = require('@scandipwa/scandipwa-dev-utils/logger');
 const googleAnalytics = require('@scandipwa/scandipwa-dev-utils/analytics');
+const userInteraction = require('../override/lib/util/user-interaction');
 
 module.exports = (yargs) => {
-    yargs.command('workspace', 'Create workspace file', createWorkspace);
+    yargs.command('workspace', 'Create workspace file', () =>
+        createWorkspace(userInteraction)
+    );
 };
--- build-packages/scandipwa-development-toolkit-core/src/workspace/index.ts
+++ build-packages/scandipwa-development-toolkit-core/src/workspace/index.ts
@@ -1,23 +1,35 @@
-import { MosaicConfig, getMosaicConfig } from "@tilework/mosaic-dev-utils/mosaic-config";
 import { IUserInteraction } from "../types";
-import { WorkspaceConfiguration, WorkspaceFolder } from "vscode";
 const { getPackageJson } = require("@scandipwa/scandipwa-dev-utils/package-json");
 const path = require('path');
 const logger = require('@scandipwa/scandipwa-dev-utils/logger');
 const { getParentThemePaths } = require('@tilework/mosaic-dev-utils/parent-theme');
 const { walkDirectoryUp, contextTypes: { THEME_TYPE } } = require('@tilework/mosaic-dev-utils/get-context');
 import * as fs from 'fs';
+import { VS_CODE_RECOMMENDATIONS } from "./suggestedVSCodeExtensions";
+const { getExtensionsForCwd } = require('@tilework/mosaic-dev-utils/extensions-core');
 
+//TODO: Add CPWA after release
 const corePathnames = ['@scandipwa/scandipwa'];
 
-const createFolder = (path: string, name: string) => {
+type ExtensionType = {
+    packageName: string;
+    packagePath: string;
+};
+
+type FolderType = {
+    path: string;
+    name?: string;
+};
+
+const createFolder = (path: string, name?: string): FolderType => {
     return {
-        path: path,
-        name: corePathnames.includes(name) ? 'Core ScandiPWA' : name,
+        path,
+        // JSON.stringify filters the undefined values
+        name: name ? corePathnames.includes(name) ? 'Core ScandiPWA' : name : undefined,
     };
 };
 
-const createWorkspace = (userInteraction: IUserInteraction) => {
+const createWorkspace = async (userInteraction: IUserInteraction) => {
     try {
         const { type: context, pathname: themePathname } = walkDirectoryUp(process.cwd(), THEME_TYPE) as {
             type: string,
@@ -52,14 +64,34 @@ const createWorkspace = (userInteraction: IUserInteraction) => {
 
         const themeFolders = [currentThemeWorkspaceFolder, ...parentThemeFolders];
 
+        // This helper functions gets extensions for parent themes as well
+        const allExtensions: ExtensionType[] = getExtensionsForCwd(currentThemePath).reverse();
+
+        const includedExtensions = await userInteraction.multiSelect(
+            `Select wanted extensions from theme `,
+            allExtensions.map(({ packageName, packagePath }) => ({
+                displayName: packageName,
+                value: packagePath
+            }))
+        ) ?? [];
+
+        const extensionsFolders = includedExtensions.map(ext => createFolder(ext, ext.includes('node_modules') ? ext.split("/").pop() + " (NO_EDIT - from node_modules)" : undefined));
+
         const workspace = {
             folders: [
-                ...themeFolders
-            ]
+                ...themeFolders,
+                ...extensionsFolders
+            ],
+            extensions: {
+                recommendations: VS_CODE_RECOMMENDATIONS,
+            }
         };
 
-        fs.writeFileSync(path.join(currentThemePath, `${name}.code-workspace`), JSON.stringify(workspace, null, 4), { encoding: 'utf8' });
+        const workspaceFile = path.join(currentThemePath, `${name}.code-workspace`);
 
+        fs.writeFileSync(workspaceFile, JSON.stringify(workspace, null, 4), { encoding: 'utf8' });
+
+        logger.logN(`Workspace file has been created at ${logger.style.file(workspaceFile)}`);
     }
     catch (e) {
         logger.note(e);
