0655a6d4d182c574abd802ec2d500a39636a7f40
mohamed.khaled <mohamed.khaled@scandiweb.com>
2024-04-14 19:58:43 +0200

    PERF-49 - added sharedTransition component and fixed sharedTransition animation when going from PLP to PDP

---
+++ packages/scandipwa/src/component/SharedTransition/SharedTransition.component.tsx
@@ -0,0 +1,125 @@
+// @ts-nocheck
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/scandipwa
+ * @link https://github.com/scandipwa/scandipwa
+ */
+
+import { createRef, PureComponent } from 'react';
+
+import { ReactElement } from 'Type/Common.type';
+
+import { SHARED_ELEMENT_TRANSITION } from './SharedTransition.config';
+import { SharedTransitionComponentProps } from './SharedTransition.type';
+
+import './SharedTransition.style';
+
+/** @namespace Component/SharedTransition/Component */
+export class SharedTransitionComponent extends PureComponent<SharedTransitionComponentProps> {
+    sharedContainer = createRef<HTMLDivElement>();
+
+    animationSpeed = SHARED_ELEMENT_TRANSITION;
+
+    transitionInAction = false;
+
+    setDestinationTransform = this.setTransform.bind(this, 'destinationPosition');
+
+    setStartingTransform = this.setTransform.bind(this, 'startingPosition');
+
+    __construct(props: SharedTransitionComponentProps): void {
+        super.__construct?.(props);
+
+        this.cleanUpTransition = this.cleanUpTransition.bind(this);
+    }
+
+    componentDidUpdate(): void {
+        if (this.transitionInAction) {
+            return;
+        }
+
+        this.updateSharedElement();
+    }
+
+    setTransform(key: 'startingPosition' | 'destinationPosition'): void {
+        const {
+            state: {
+                [key]: {
+                    width,
+                    height,
+                    left,
+                    top,
+                },
+            },
+        } = this.props;
+
+        if (this.sharedContainer.current) {
+            this.sharedContainer.current.style.cssText = `
+                --shared-element-width: ${width}px;
+                --shared-element-height: ${height}px;
+                --shared-element-top: ${top}px;
+                --shared-element-start: ${left}px;
+                --shared-element-animation-speed: ${this.animationSpeed}ms;
+            `;
+        }
+    }
+
+    cleanUpTransition(): void {
+        const { current: wrapper } = this.sharedContainer;
+        const { cleanUpTransition } = this.props;
+
+        if (wrapper) {
+            const range = document.createRange();
+
+            range.selectNodeContents(wrapper);
+            range.deleteContents();
+        }
+
+        this.transitionInAction = false;
+        cleanUpTransition();
+    }
+
+    updateSharedElement(): void {
+        const {
+            state: {
+                sharedElementDestination,
+                sharedElement,
+            },
+        } = this.props;
+
+        const { current: wrapper } = this.sharedContainer;
+
+        if (
+            !sharedElement
+            || !sharedElementDestination
+            || !wrapper
+        ) {
+            // this.cleanUpTransition();
+            return;
+        }
+
+        this.transitionInAction = true;
+        this.setStartingTransform();
+        wrapper.appendChild(sharedElement);
+        setTimeout(this.setDestinationTransform, 0);
+        setTimeout(this.cleanUpTransition, this.animationSpeed);
+    }
+
+    render(): ReactElement {
+        const { state: { sharedElement } } = this.props;
+
+        return (
+            <div
+              block="SharedTransition"
+              mods={ { isVisible: !!sharedElement } }
+              ref={ this.sharedContainer }
+            />
+        );
+    }
+}
+
+export default SharedTransitionComponent;
---
+++ packages/scandipwa/src/component/SharedTransition/SharedTransition.unstated.ts
@@ -0,0 +1,78 @@
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/scandipwa
+ * @link https://github.com/scandipwa/scandipwa
+ */
+
+import { RefObject } from 'react';
+import { Container } from 'unstated-typescript';
+
+import { HEADER_HEIGHT, PRODUCT_GALLERY_SLIDER_CLASS_NAME } from './SharedTransition.config';
+import { SharedTransitionPosition, SharedTransitionState } from './SharedTransition.type';
+
+export const sharedTransitionInitialState: SharedTransitionState = {
+    sharedElementDestination: null,
+    sharedElement: null,
+    destinationPosition: {},
+    startingPosition: {},
+};
+
+/** @namespace Component/SharedTransition/Unstated */
+export class SharedTransitionUnstated extends Container<SharedTransitionState> {
+    state: SharedTransitionState = sharedTransitionInitialState;
+
+    __construct(): void {
+        this.registerSharedElementDestination = this.registerSharedElementDestination.bind(this);
+        this.registerSharedElement = this.registerSharedElement.bind(this);
+    }
+
+    _parseRectangle(val: DOMRect): SharedTransitionPosition {
+        return JSON.parse(JSON.stringify(val));
+    }
+
+    // eslint-disable-next-line @scandipwa/scandipwa-guidelines/no-arrow-functions-in-class
+    cleanUpTransition = (): void => {
+        this.setState(sharedTransitionInitialState);
+    };
+
+    registerSharedElementDestination({ current }: RefObject<HTMLElement>): void {
+        if (current) {
+            const isPdpDestination = current.classList.contains(PRODUCT_GALLERY_SLIDER_CLASS_NAME);
+
+            this.setState(({ sharedElementDestination }) => {
+                if (sharedElementDestination) {
+                    return {};
+                }
+
+                return {
+                    sharedElementDestination: current,
+                    destinationPosition: isPdpDestination ? {
+                        ...this._parseRectangle(current.getBoundingClientRect()),
+                        // to make sure animation will end at the correct position
+                        top: HEADER_HEIGHT,
+                    } : this._parseRectangle(current.getBoundingClientRect()),
+                };
+            });
+        }
+    }
+
+    registerSharedElement({ current }: RefObject<HTMLElement>): void {
+        if (current) {
+            const clone = current.cloneNode(true) as HTMLElement;
+
+            this.setState({
+                sharedElement: clone,
+                sharedElementDestination: null,
+                destinationPosition: {},
+                startingPosition: this._parseRectangle(current.getBoundingClientRect()),
+            });
+        }
+    }
+}
+
+export default new SharedTransitionUnstated();
